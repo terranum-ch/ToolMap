b35ddbc229f6526db014836dbd3d5b9dac44bca2
lucsch <lucsch@8d3dc7bd-f08f-3943-bf3e-8d26abd385ca>
2007-12-01 18:04:37 +0000

    changing the script for numbering versions

--- trunk/build/version_svn_unix.sh
+++ trunk/build/version_svn_unix.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 #script for getting the last svn version number 
 #works only for unix like (mac, linux)
-#    echo  'const char* svn_version(void) { const char* SVN_Version ="\c' > ../src/core/svn_version.cpp
-	echo '#define SVN_VERSION _T("\c' > ../src/core/svn_version.h
+	echo -n '#define SVN_VERSION _T("' > ../src/core/svn_version.h
     svnversion -n                  >> ../src/core/svn_version.h
     echo '")'   >> ../src/core/svn_version.h
     echo '' >> ../src/core/svn_version.h
