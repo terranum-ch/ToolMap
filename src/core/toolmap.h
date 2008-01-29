77a04360b4bb4f4c6ed14939fcd0aa10ce27d009
lucsch <lucien.schreiber@terranum.ch>
2008-01-29 13:43:30 +0000

    test for concurent modifications (Mac)

--- src/core/toolmap.h
+++ src/core/toolmap.h
@@ -78,7 +78,7 @@
 #include "../gui/tocwindow_dlg.h"		// Table of content (TOC) definition
 #include <wx/sysopt.h>
 #include "../database/db_project.h"
-#include "managed_aui_wnd.h" // temp 
+#include "managed_aui_wnd.h" // temp test for concurent modif
 
 #include <wx/mstream.h>
 
