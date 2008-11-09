6b65669fb03d03f2720d991678d61cea92a88537
lucsch <lucsch@8d3dc7bd-f08f-3943-bf3e-8d26abd385ca>
2008-11-09 15:36:46 +0000

    Adding code for queries

---
+++ trunk/src/gui/queries_panel_dlg.cpp
@@ -0,0 +1,95 @@
+/***************************************************************************
+								queries_panel_dlg.cpp
+                    Display dialog for queries panel (simple version)
+                             -------------------
+    copyright            : (C) 2007 CREALP Lucien Schreiber 
+    email                : lucien.schreiber at crealp dot vs dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+// comment doxygen
+
+#include "queries_panel_dlg.h"
+
+
+/***************************************************************************//**
+ @brief Constructor
+ @details Create all the controls
+ @author Lucien Schreiber (c) CREALP 2008
+ @date 09 November 2008
+ *******************************************************************************/
+QueriesListDLG::QueriesListDLG( wxWindow* parent, wxWindowID id,
+							   const wxString& title, const wxPoint& pos,
+							   const wxSize& size, long style ) 
+: wxDialog( parent, id, title, pos, size, style )
+{
+	this->SetSizeHints( wxDefaultSize, wxDefaultSize );
+	
+	wxBoxSizer* bSizer2;
+	bSizer2 = new wxBoxSizer( wxVERTICAL );
+	
+	wxBoxSizer* bSizer3;
+	bSizer3 = new wxBoxSizer( wxHORIZONTAL );
+	
+	wxStaticText* m_staticText2;
+	m_staticText2 = new wxStaticText( this, wxID_ANY, wxT("Description"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText2->Wrap( -1 );
+	bSizer3->Add( m_staticText2, 0, wxALL, 5 );
+	
+	m_QueriesName = new wxTextCtrl( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize( 200,-1 ), 0 );
+	m_QueriesDescription->SetMaxLength( 255 ); 
+	bSizer3->Add( m_QueriesDescription, 0, wxALL, 5 );
+	
+	bSizer2->Add( bSizer3, 0, wxALL|wxEXPAND, 5 );
+	
+	wxStaticBoxSizer* sbSizer2;
+	sbSizer2 = new wxStaticBoxSizer( new wxStaticBox( this, wxID_ANY, wxT("Queries (SQL Select statement)") ), wxVERTICAL );
+	
+	m_QueriesDescription = new wxTextCtrl( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize( -1,100 ), 0 );
+	sbSizer2->Add( m_QueriesDescription, 1, wxALL|wxEXPAND, 5 );
+	
+	bSizer2->Add( sbSizer2, 1, wxALL|wxEXPAND, 5 );
+	
+	wxStaticLine* m_staticline1;
+	m_staticline1 = new wxStaticLine( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxLI_HORIZONTAL );
+	bSizer2->Add( m_staticline1, 0, wxEXPAND | wxALL, 5 );
+	
+	wxBoxSizer* bSizer4;
+	bSizer4 = new wxBoxSizer( wxHORIZONTAL );
+	
+	wxButton* m_button3;
+	m_button3 = new wxButton( this, ID_DLG_QUERIES_ADD, wxT("&Add"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_button3->SetDefault(); 
+	bSizer4->Add( m_button3, 0, wxALL, 5 );
+	
+	wxButton* m_button4;
+	m_button4 = new wxButton( this, ID_DLG_QUERIES_CLOSE, wxT("&Close"), wxDefaultPosition, wxDefaultSize, 0 );
+	bSizer4->Add( m_button4, 0, wxALL, 5 );
+	
+	bSizer2->Add( bSizer4, 0, wxALIGN_RIGHT, 5 );
+	
+	this->SetSizer( bSizer2 );
+	this->Layout();
+	bSizer2->Fit( this );
+}
+
+
+
+/***************************************************************************//**
+ @brief Destructor
+ @author Lucien Schreiber (c) CREALP 2008
+ @date 09 November 2008
+ *******************************************************************************/
+QueriesListDLG::~QueriesListDLG()
+{
+}
+
+
