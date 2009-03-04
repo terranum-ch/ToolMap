13b96bee1e7b8dacd567f4afd8a3d2d094d2c12c
lucsch <lucien.schreiber@terranum.ch>
2009-03-04 16:34:37 +0000

    - Adding wxTreeMultiCtrl control for advanced attribution - Moving wxSerialize into new components directory

---
+++ src/components/wxtreemultictrl/TreeMultiItemBase.h
@@ -0,0 +1,217 @@
+//---------------------------------------------------------------------------
+// $RCSfile: TreeMultiItemBase.h $
+// $Source: contrib/include/wx/treemultictrl/TreeMultiItemBase.h $
+// $Revision: 1.84 $
+// $Date: Jul 29, 2007 2:09:31 PM $
+//---------------------------------------------------------------------------
+// Author:      Jorgen Bodde
+// Copyright:   (c) Jorgen Bodde
+// License:     wxWidgets License
+//---------------------------------------------------------------------------
+
+#ifndef __TREEMULTIITEMBASE_HPP_
+#define __TREEMULTIITEMBASE_HPP_
+
+#ifdef __GNUG__
+    #pragma interface "TreeMultiItemBase.cpp"
+#endif
+
+#ifndef WX_PRECOMP
+    #include "wx/wx.h"
+#endif
+
+// forward definitions
+class TreeMultiItemRoot;
+class TreeMultiItemWindow;
+class TreeMultiItemNode;
+
+class TreeMultiItemBase
+{
+protected:
+	TreeMultiItemBase(TreeMultiItemNode *parent);
+
+	/** Name property of this item, useful for assigning / coupling
+	    external variable reference to this item. It is not mandatory */
+	wxString _name;
+
+	/** Flag to indicate that this node is visible or not. The Node can be 
+	    temporarily excluded by flagging it. This has also consequences for all 
+		the children of this node (if any) */
+	bool _excluded;
+
+	/** Type of instanced class */
+	int _type;
+
+	/** Calculated X, Y position */
+	int _x, _y;
+
+	/** Calculated width, height */
+	int _width, _height;
+	
+#if(CHECKBOXVIEW)
+	/** Checkbox flag (draw one or not) */
+	bool _checkbox;
+	
+	/** Internal draw state. This is a unintelligent state, and should be
+	    updated when this node has some checked and some unchecked children
+		then this should become a tri-stated item */
+	int _checkboxState;
+#endif
+
+public:
+	virtual ~TreeMultiItemBase();
+
+	// type of tree item
+
+    int GetType() const { return _type; };
+	TreeMultiItemNode *GetParent() const { return _parent; };
+
+	/** Get name of this node */
+        wxString GetName() const {
+                return _name;
+        };
+ /** Set name of the item */
+  void SetName(wxString const& NewName)
+  {
+    this->_name = NewName;
+  }
+
+        /** returns the instance pointer if the current node is
+        a TreeMultiItemRoot, and NULL when it's not.
+	*/
+	virtual TreeMultiItemRoot *IsTreeMultiItemRoot() const {
+		return 0;
+	};
+
+    /** returns the instance pointer if the current node is
+        a TreeMultiItemWindow, and NULL when it's not.
+	*/
+	virtual TreeMultiItemWindow *IsTreeMultiItemWindow() const {
+		return 0;
+	};
+
+    /** returns the instance pointer if the current node is
+        a TreeMultiItemNode, and NULL when it's not.
+	*/
+	virtual TreeMultiItemNode *IsTreeMultiItemNode() const {
+		return 0;
+	};
+
+	/** Sets or resets the excluded flag. When excluded node is not visible */
+	void SetExcluded(bool excluded) {
+		_excluded = excluded;
+	};
+
+	/** Get / Set routine for X */
+	void SetX(int x) {
+		_x = x;
+	};
+
+	int GetX() const {
+		return _x;
+	};
+
+	/** Get / Set routine for Y */
+	void SetY(int y) {
+		_y = y;
+	};
+
+	int GetY() const {
+		return _y;
+	};
+
+	/** Get / Set routine for height */
+	void SetHeight(int height) {
+		_height = height;
+	};
+
+	int GetHeight() const {
+		return _height;
+	};
+
+	/** Get / Set routine for width */
+	void SetWidth(int width) {
+		_width = width;
+	};
+
+	int GetWidth() const {
+		return _width;
+	};
+
+	/** Returns true when this item is drawn somewhere in the
+	    tree. Whenever a parent of this node is collapsed, it
+	    is not visible and it is not necessary to perform i.e.
+	    redraw actions. 
+		
+		It also returns false when this node is excluded from the
+		tree. 
+		
+		\sa wxTreeMultiCtrl::Exclude(), wxTreeMultiCtrl::Include())
+	*/
+	bool IsVisible();
+
+	/* Returns if this node is excluded from the tree. If this item
+	   is a Node, then all kids are excluded as well */
+	bool IsExcluded() const {
+                return _excluded;
+        };
+        
+ //
+ // item selection status handling
+ 
+ /** checks if the item is selected */
+  bool IsSelected(void) const
+  {
+    return this->m_Selected;
+  }
+
+ /** mark the current item as selected */
+  void Select(void)
+  {
+    this->m_Selected = true;
+  }
+  
+ /** toggle the selection status */
+  void ToggleSelection(void)
+  {
+    this->m_Selected = !(this->m_Selected);
+  }
+
+ /** unmark the item */
+  void Unselect(void)
+  {
+    this->m_Selected = false;
+  }
+
+#if(CHECKBOXVIEW)
+        /** Sets checkbox or not. This does not influence the state of the checkbox */
+        void SetCheckbox(bool value) {
+		_checkbox = value;
+	};
+
+	/** Returns current state of the checkbox view */
+	bool GetCheckbox() const {
+		return _checkbox;
+	};
+
+	/** Sets checkbox state. If 0 it's unchecked, 1 = checked and 2 = tristate */
+	virtual void SetCheckboxState(int state) {
+		wxCHECK2(state < 3 && state >= 0, return);
+		_checkboxState = state;
+	};
+
+	/** Returns current state of checkbox */
+	int GetCheckboxState() const {
+		return _checkboxState;
+	};
+#endif
+
+private:
+    TreeMultiItemNode *_parent;
+        
+ 	// flags to indicate the status of the item:
+  	bool m_Selected;
+};
+
+#endif
+
---
+++ src/components/wxtreemultictrl/TreeMultiItemNode.cpp
@@ -0,0 +1,196 @@
+//---------------------------------------------------------------------------
+// $RCSfile$
+// $Source$
+// $Revision$
+// $Date$
+//---------------------------------------------------------------------------
+// Author:      Jorgen Bodde
+// Copyright:   (c) Jorgen Bodde
+// License:     wxWidgets License
+//---------------------------------------------------------------------------
+
+#ifdef __GNUG__
+    #pragma implementation "TreeMultiItemNode.cpp"
+#endif
+
+/* for compilers that support precompilation
+   includes "wx/wx.h" */
+
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+    #pragma hdrstop
+#endif
+
+#include "TreeMultiItemNode.h"
+
+#include "wx/arrimpl.cpp"
+WX_DEFINE_OBJARRAY(TreeMultiItemBaseArray);
+
+/** TreeMultiItemNode
+  * This class is a container holder for multiple TreeMultiItemBase classes.
+  * Since both a TreeMultiItemNode and a TreeMultiItemWindow are a descendant
+  * from a TreeMultiItemBase class, it can hold multiple of these.
+  * In the case of a directory tree structure, it can hold multiple
+  * directories, and leafs (files).
+  */
+
+// default constructor for container composite
+TreeMultiItemNode::TreeMultiItemNode(TreeMultiItemNode *parent, const wxString &caption, const wxString &name)
+	: TreeMultiItemBase(parent)
+	, _caption(caption)
+	, _nodeExpanded(true)
+
+{
+	_name = name;
+	Clear();
+}
+
+//------------------------------------------------------------
+
+TreeMultiItemNode::~TreeMultiItemNode()
+{
+    // delete all items on this level
+    Clear();
+}
+
+//------------------------------------------------------------
+
+void TreeMultiItemNode::AddNode(TreeMultiItemBase *node)
+{
+	if(node)
+                _items.Add(node);
+}
+
+void TreeMultiItemNode::InsertNode(TreeMultiItemBase* NodePtr, size_t Position)
+{
+        if (NodePtr != NULL)
+                this->_items.Insert(NodePtr,Position);
+}
+
+//------------------------------------------------------------
+
+void TreeMultiItemNode::DeleteNode(TreeMultiItemBase *node)
+{
+	// this is wrong. The RemoveAt and Remove should delete
+	// the object
+	if(node)
+	{
+		_items.Detach(Index(node));
+		delete node;
+	}
+
+}
+
+//------------------------------------------------------------
+
+void TreeMultiItemNode::DeleteNode(int index)
+{
+	// this is wrong. The RemoveAt and Remove should delete
+	// the object
+	if(index < (int)_items.Count() && index >= 0)
+		_items.RemoveAt(index);
+}
+
+//------------------------------------------------------------
+
+int TreeMultiItemNode::GetNodeCount() const
+{
+	return _items.Count();
+}
+
+//------------------------------------------------------------
+
+TreeMultiItemBase *TreeMultiItemNode::GetNode(int index) const
+{
+	TreeMultiItemBase *value = 0;
+
+	if(index < (int)_items.Count() && index >= 0)
+		value = &_items.Item(index);
+
+	return value;
+}
+
+//------------------------------------------------------------
+
+TreeMultiItemBase *TreeMultiItemNode::RemoveNode(TreeMultiItemBase *node)
+{
+	if(node)
+		_items.Detach(_items.Index(*node));
+
+	return node;
+}
+
+//------------------------------------------------------------
+
+TreeMultiItemBase * TreeMultiItemNode::RemoveNode(int index)
+{
+	TreeMultiItemBase *value = 0;
+
+	if(index < (int)_items.Count() && index > 0)
+	{
+		value = &_items.Item(index);
+		_items.Detach(index);
+	}
+
+	return value;
+}
+
+//------------------------------------------------------------
+
+int TreeMultiItemNode::Index(TreeMultiItemBase *node,  bool searchFromEnd) const
+{
+	wxCHECK(node, -1);
+
+	return _items.Index(*node, searchFromEnd);
+}
+
+//------------------------------------------------------------
+
+void  TreeMultiItemNode::Clear()
+{
+	_items.Clear();
+}
+
+//------------------------------------------------------------
+
+TreeMultiItemBase * TreeMultiItemNode::GetNodeNext(int &cookie) const
+{
+	TreeMultiItemBase *value = 0;
+
+	if(cookie >= 0 && cookie < (int)_items.Count())
+	{
+		value = &_items[cookie];
+		cookie++;
+	}
+
+	return value;
+}
+
+//------------------------------------------------------------
+
+TreeMultiItemBase* TreeMultiItemNode::First() const
+{
+  if (this->_items.GetCount() > 0)
+    return &(this->_items[0]);
+  else
+    return NULL;
+} /* TreeMultiItemNode::First() const */
+
+TreeMultiItemBase* TreeMultiItemNode::Last() const
+{
+        if(this->_items.GetCount() > 0)
+          return &(this->_items.Last());
+        else
+          return NULL;
+} /* TreeMultiItemNode::Last() const */
+
+
+//------------------------------------------------------------
+
+void TreeMultiItemNode::SetCaption(const wxString &caption)
+{
+     /* this is an uncontrolled set, please make sure that
+        the main wxTreeMultiCtrl gets a RedrawFromNode */
+     _caption = caption;
+}
