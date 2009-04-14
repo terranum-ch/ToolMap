9b2b054658889c2d397513ba5189ce08389a7695
lucsch <lucsch@8d3dc7bd-f08f-3943-bf3e-8d26abd385ca>
2009-04-14 19:47:26 +0000

    Adding framework for unit testing. Will refactorize database class

---
+++ trunk/test/src_test/database_test.h
@@ -0,0 +1,39 @@
+/***************************************************************************
+								database_test.h
+							UNIT TEST for database
+                             -------------------
+    copyright            : (C) 2009 CREALP Lucien Schreiber 
+    email                : lucien.schreiber at crealp dot vs dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+
+#ifndef _DATABASE_TEST_H_
+#define _DATABASE_TEST_H_
+
+/*
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+// Include wxWidgets' headers
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif*/
+
+
+
+
+
+
+
+
+#endif
