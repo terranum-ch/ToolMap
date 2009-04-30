f9d2983f3f372cf6dcae862c2d1070317b758719
lucsch <lucsch@8d3dc7bd-f08f-3943-bf3e-8d26abd385ca>
2009-04-30 09:13:58 +0000

    Correcting following windows scripts: - svnversion script - installer script - ToolMap updater script

--- trunk/build/version_svn_win.bat
+++ trunk/build/version_svn_win.bat
@@ -1,7 +1,7 @@
 @ECHO OFF 
 REM script for getting the last svn version number 
 REM works only for windows
-
+cd ..
 FOR /F "usebackq" %%s IN (`svnversion .`) DO @SET REV=%%s
  
-ECHO  #define SVN_VERSION _T(^"%REV%^") >../src/core/svn_version.h
+ECHO  #define SVN_VERSION _T(^"%REV%^") > src/core/svn_version.h
