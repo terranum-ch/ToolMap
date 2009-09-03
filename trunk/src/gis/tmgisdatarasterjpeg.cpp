e2af00011ebafeadc298f987177f63800c321ff1
lucsch <lucsch@8d3dc7bd-f08f-3943-bf3e-8d26abd385ca>
2009-09-03 10:25:14 +0000

    Feature #55 Adding support for jpeg raster

---
+++ trunk/src/gis/tmgisdatarasterjpeg.cpp
@@ -0,0 +1,54 @@
+/***************************************************************************
+							tmgisdatarasterjpeg.cpp
+                   For dealing with spatial raster data (*.jpg, *.jpeg)
+                             -------------------
+    copyright            : (C) 2009 CREALP Lucien Schreiber 
+    email                : lucien.schreiber at crealp dot vs dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+// comment doxygen
+
+#include "tmgisdatarasterjpeg.h"
+
+
+tmGISDataRasterJPEG::tmGISDataRasterJPEG()
+{
+	m_FileType = _T("GDAL JPEG Raster");
+}
+
+tmGISDataRasterJPEG::~tmGISDataRasterJPEG()
+{
+	
+}
+
+
+/***************************************************************************//**
+ @brief Get Size of The Jpeg raster
+ @param iPrecision The number of digits required (default is 2)
+ @return  An html compliant string containing the Jpeg size in an human
+ readable format
+ @author Lucien Schreiber (c) CREALP 2008
+ @date 24 October 2008
+ *******************************************************************************/
+wxString tmGISDataRasterJPEG::GetDataSizeAsHtml (int iPrecision)
+{
+	wxString myResult = _("<U><B>Jpeg Raster Size</B></U><BR>");
+	
+	wxFileName myJpgName (GetFullFileName());
+	myResult.Append(wxString::Format(_("Raster size : %s"),
+									 myJpgName.GetHumanReadableSize(_T("Error computing raster size"),
+																	 iPrecision).c_str()));
+	
+	return myResult;
+}
+
+
---
+++ trunk/src/gis/tmgisdatarasterjpeg.h
@@ -0,0 +1,55 @@
+/***************************************************************************
+							tmgisdatarasterjpeg.h
+						For dealing with JPEG raster
+                             -------------------
+    copyright            : (C) 2009 CREALP Lucien Schreiber 
+    email                : lucien.schreiber at crealp dot vs dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+// comment doxygen
+
+
+#ifndef _TM_GISDATARASTER_JPEG_H_
+#define _TM_GISDATARASTER_JPEG_H_
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+// Include wxWidgets' headers
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
+
+#include "tmgisdataraster.h"
+#include "gdal_priv.h"	
+
+
+
+class tmGISDataRasterJPEG : public tmGISDataRaster
+	{
+	private:
+		
+	protected:
+	public:
+		tmGISDataRasterJPEG();
+		~tmGISDataRasterJPEG();
+		
+		virtual wxString GetDataSizeAsHtml (int iPrecision = 2);
+		
+	};
+
+
+
+
+
+
+#endif
