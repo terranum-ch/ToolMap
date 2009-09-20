bfc717c461cd88059c83daefb9b68c8a70c40051
lucsch <lucien.schreiber@terranum.ch>
2009-09-20 20:17:18 +0000

    Adding class for saving / loading windows position with support for Screen size change

---
+++ src/core/tmwindowposition.cpp
@@ -0,0 +1,212 @@
+/***************************************************************************
+								tmwindowposition.cpp
+						Save and load windows position
+                             -------------------
+    copyright            : (C) 2007 CREALP Lucien Schreiber 
+    email                : lucien.schreiber at crealp dot vs dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+// comment doxygen
+
+#include "tmwindowposition.h"
+
+
+tmWindowPosition::tmWindowPosition()
+{
+	WP_LoadScreenSize();
+	//InitMemberValues();
+}
+
+
+
+/*tmWindowPosition::tmWindowPosition(wxSize screensize)
+{
+	InitMemberValues();
+	Create(screensize);
+}*/
+
+
+/*void tmWindowPosition::Create (wxSize screensize)
+{
+	m_ScreenSize = screensize;
+}*/
+
+
+/*void tmWindowPosition::InitMemberValues()
+{
+	//m_ScreenSize = wxDefaultSize;
+}*/
+
+
+tmWindowPosition::~tmWindowPosition()
+{
+	WP_SaveScreenSize();
+}
+
+
+/*bool tmWindowPosition::WP_IsInited()
+{
+	if (m_ScreenSize != wxDefaultSize)
+		return true;
+	
+	wxLogDebug(_T("Windows position not correctly inited"));
+	
+	return false;
+}*/
+
+
+bool tmWindowPosition::LoadPosition(const wxString & wndname, wxRect & pos)
+{
+	//if (WP_IsInited()==false)
+	//	return false;
+	
+	wxString myMainPosition = wxEmptyString;
+	
+	wxFileConfig myConfig;
+	myConfig.SetPath(_T("WINDOW_POSITION"));
+	uint myNumberOfWindows = myConfig.GetNumberOfEntries();
+	if (myNumberOfWindows == 0)
+	{
+		wxLogDebug(_T("No position stored"));
+		return false;
+	}
+	
+	wxString readedpos = wxEmptyString;
+	if (myConfig.Read(wndname, &readedpos)==false)
+	{
+		wxLogError(_("No position stored for windows %s"), wndname.c_str());
+		return false;
+	}
+	
+	pos =  WP_StringToPosition(readedpos);
+	
+	wxRect myActualSize = WP_GetActualScreenSize();
+	if (wndname != _T("SCREEN") && HasScreenChanged()==true)
+	{
+		if (Intersects(pos, wxSize(myActualSize.GetWidth(), myActualSize.GetHeight()))==false)
+		{
+			wxLogDebug(_T("Windows %s was outside screen"), wndname.c_str());
+			return false;
+		}
+	}
+	return true;
+}
+
+
+
+wxString tmWindowPosition::WP_PositionToString(wxRect pos)
+{
+	wxString mySPos = wxString::Format(_T("%d;%d;%d;%d"),
+									   pos.GetX(),
+									   pos.GetY(),
+									   pos.GetWidth(),
+									   pos.GetHeight());
+	return mySPos;
+}
+
+
+
+wxRect tmWindowPosition::WP_StringToPosition(const wxString & posstring)
+{
+	wxStringTokenizer tokenizer(posstring, _T(";"));
+	wxASSERT(tokenizer.CountTokens() == 4);
+
+	long myTemp[] = {wxNOT_FOUND, wxNOT_FOUND, 
+					wxNOT_FOUND, wxNOT_FOUND};
+	wxRect myRect;
+	
+	for (int i = 0; i<4; i++)
+    {
+        tokenizer.GetNextToken().ToLong(&(myTemp[i]));
+    }
+	
+	myRect.SetX(myTemp[0]);
+	myRect.SetY(myTemp[1]);
+	myRect.SetWidth(myTemp[2]);
+	myRect.SetHeight(myTemp[3]);
+	return myRect;
+}
+
+
+
+
+bool tmWindowPosition::SavePosition(const wxString & wndname, wxRect pos)
+{
+	wxString myStringPosition = wxEmptyString;
+	wxFileConfig myConfig;
+	
+	myConfig.SetPath(_T("WINDOW_POSITION"));
+	if (myConfig.Write(wndname, WP_PositionToString(pos))==false)
+	{
+		wxLogDebug(_T("Unable to write to config file"));
+		return false;
+	}
+	
+	return true;
+	
+}
+
+
+
+bool tmWindowPosition::WP_SaveScreenSize ()
+{
+	wxRect myWndSize = WP_GetActualScreenSize();
+	return SavePosition(_T("SCREEN"), myWndSize);
+}
+
+
+
+bool tmWindowPosition::WP_LoadScreenSize ()
+{
+	wxRect myWndSize = WP_GetActualScreenSize();
+	if (LoadPosition(_T("SCREEN"), myWndSize)==false)
+	{
+		m_OldScreenSize = wxSize(myWndSize.GetWidth(), myWndSize.GetHeight());
+		return false;
+	}
+	
+	m_OldScreenSize = wxSize(myWndSize.GetWidth(), myWndSize.GetHeight());
+	return true;
+}
+
+
+bool tmWindowPosition::HasScreenChanged()
+{
+	wxRect myActualSize = WP_GetActualScreenSize();
+	if (m_OldScreenSize != wxSize(myActualSize.GetWidth(), myActualSize.GetHeight()))
+	{
+		wxLogDebug(_T("Screen size has changed"));
+		return true;
+	}
+	
+	return false;
+}
+
+
+
+
+wxRect tmWindowPosition::WP_GetActualScreenSize()
+{
+	return wxRect(0,0,
+				  wxSystemSettings::GetMetric(wxSYS_SCREEN_X , NULL),
+				  wxSystemSettings::GetMetric(wxSYS_SCREEN_Y , NULL));
+}
+	
+
+bool tmWindowPosition::Intersects (wxRect wndpos, wxSize screensize)
+{
+	wxRect myScreenSize = wxRect(wxPoint(0,0),screensize);
+	return myScreenSize.Intersects(wndpos);
+}
+
+
+		
---
+++ src/core/tmwindowposition.h
@@ -0,0 +1,70 @@
+/***************************************************************************
+							tmwindowposition.h
+						Save and load windows position
+                             -------------------
+    copyright            : (C) 2007 CREALP Lucien Schreiber 
+    email                : lucien.schreiber at crealp dot vs dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+// comment doxygen
+
+
+#ifndef _TM_WINDOWS_POSITION_H_
+#define _TM_WINDOWS_POSITION_H_
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+// Include wxWidgets' headers
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
+
+#include <wx/fileconf.h>
+
+
+
+class tmWindowPosition : public wxObject
+	{
+	private:
+		wxSize m_OldScreenSize;
+		//wxSize m_ScreenSize;
+		//wxFileConfig m_Config;
+		
+		//void InitMemberValues();
+		//bool WP_IsInited();
+		wxString WP_PositionToString(wxRect pos);
+		wxRect WP_StringToPosition(const wxString & posstring);
+		bool WP_SaveScreenSize ();
+		bool WP_LoadScreenSize ();
+		wxRect WP_GetActualScreenSize();
+		
+	protected:
+	public:
+		tmWindowPosition();
+		//tmWindowPosition(wxSize screensize);
+		//void Create (wxSize screensize);
+		~tmWindowPosition();
+		
+		bool LoadPosition(const wxString & wndname, wxRect & pos);
+		bool SavePosition(const wxString & wndname, wxRect pos); 
+		
+		bool HasScreenChanged();
+		bool Intersects (wxRect wndpos, wxSize screensize);
+};
+
+
+
+
+
+
+#endif
