1d0e83e8c0832ed785e0e0bc1ec1eee6b45547bc
lucsch <lucsch@8d3dc7bd-f08f-3943-bf3e-8d26abd385ca>
2009-09-23 16:48:14 +0000

    Adding preliminary code (and tests) for interactive point orientation tool

---
+++ trunk/src/gis/tmpointorient.cpp
@@ -0,0 +1,96 @@
+/***************************************************************************
+								tmpointorient.cpp
+                    for dealing with point orientation
+                             -------------------
+    copyright            : (C) 2009 CREALP Lucien Schreiber 
+    email                : lucien.schreiber at crealp dot vs dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+// comment doxygen
+
+#include "tmpointorient.h"
+
+
+tmPointOrient::tmPointOrient()
+{
+	InitMemberValues();
+}
+
+
+void tmPointOrient::InitMemberValues()
+{
+	m_StartPoint = wxDefaultPosition;
+	m_EndPoint = wxDefaultPosition;
+}
+
+
+
+tmPointOrient::~tmPointOrient()
+{
+	
+}
+
+
+void tmPointOrient::SetStartPoint(const wxPoint & start)
+{
+	m_StartPoint = start;
+}
+
+
+void tmPointOrient::SetEndPoint(const wxPoint & end)
+{
+	m_EndPoint = end;
+}
+
+
+bool tmPointOrient::IsValid()
+{
+	if (m_StartPoint == wxDefaultPosition ||
+		m_EndPoint == wxDefaultPosition)
+	{
+		wxLogError(_("Orientation not correctly inited"));
+		return false;
+	}
+	
+	if (m_StartPoint == m_EndPoint)
+	{
+		wxLogError(_("Not enough data for computing orientation"));
+		return false;
+	}
+	return true;
+}
+
+
+int tmPointOrient::GetOrientationInt()
+{
+	return (int) GetOrientationDouble();
+}
+
+
+double tmPointOrient::GetOrientationDouble()
+{
+	if (IsValid()==false)
+		return wxNOT_FOUND;
+	
+	// get two vectors
+	wxPoint2DDouble myVector1 = wxPoint2DDouble (m_EndPoint.x - m_StartPoint.x, m_EndPoint.y -m_StartPoint.y);
+	wxPoint2DDouble myVectorVert = wxPoint2DDouble (0, -1);
+	
+	// vector length for normalize
+	double myLength1 = myVector1.GetVectorLength();
+	myVector1 = myVector1 / myLength1;
+	
+	double myRadialVal = atan2(myVector1.m_y, myVector1.m_x) - atan2(myVectorVert.m_y, myVectorVert.m_x);
+	return 180 * myRadialVal / M_PI;
+}
+
+
---
+++ trunk/src/gis/tmpointorientattrib.h
@@ -0,0 +1,59 @@
+/***************************************************************************
+							tmpointorientattrib.h
+                    Point orientation with attribution
+                             -------------------
+    copyright            : (C) 2007 CREALP Lucien Schreiber 
+    email                : lucien.schreiber at crealp dot vs dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+// comment doxygen
+
+
+#ifndef _TM_POINT_ORIENT_ATTRIB_H_
+#define _TM_POINT_ORIENT_ATTRIB_H_
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+// Include wxWidgets' headers
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
+
+#include "tmpointorient.h"
+#include "../database/database_tm.h"
+
+
+class tmPointOrientAttrib : public tmPointOrient 
+	{
+	private:
+		long m_Oid;
+		DataBaseTM * m_DB;
+		ProjectDefMemoryFields m_OrientField;
+		
+		void InitMemberValues();
+		bool POA_IsOIDInited();
+		bool POA_IsAttributed(long & attributedvalue);
+		bool POA_HasOrientField();
+		
+	protected:
+	public:
+		tmPointOrientAttrib();
+		void Create (DataBaseTM * database,  long oid);
+		~tmPointOrientAttrib();
+		
+		virtual bool IsValid();
+};
+
+
+
+#endif
