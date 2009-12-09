4867a81bb20ee56d00b9038df9b0636d8b2b599c
lucsch <lucien.schreiber@terranum.ch>
2009-12-09 08:48:27 +0000

    Adding better build process : creating a toolmap library used for tests and for the main program (Less file to compile) Modifying tests files for using cxxtest instead of cppunit

--- test/src_test/test_database.h
+++ test/src_test/test_database.h
@@ -1,6 +1,6 @@
 /***************************************************************************
-								database_test.h
-							UNIT TEST for database
+								test_database.h
+							Testing Database class
                              -------------------
     copyright            : (C) 2009 CREALP Lucien Schreiber 
     email                : lucien.schreiber at crealp dot vs dot ch
@@ -15,20 +15,50 @@
  *                                                                         *
  ***************************************************************************/
 
+#ifndef _TEST_VR_DRIVER_H_
+#define _TEST_VR_DRIVER_H_
 
 
-#ifndef _DATABASE_TEST_H_
-#define _DATABASE_TEST_H_
-
-/*
-// For compilers that support precompilation, includes "wx/wx.h".
 #include "wx/wxprec.h"
-
-// Include wxWidgets' headers
 #ifndef WX_PRECOMP
     #include <wx/wx.h>
-#endif*/
+#endif
 
+#include "test_param.h"	//for test parameters
+#include "../../src/database/database.h"
+
+
+class TEST_Database : public CxxTest::TestSuite
+{
+private:
+	DataBase * m_DB;	
+	
+public:
+	void setUp()
+	{
+		m_DB = new DataBase();		
+	}
+	
+	void tearDown() 
+	{
+		delete m_DB;
+	}
+	
+	// setting output to the std err (otherwise log into windows)
+	void testOutputToWindows()
+	{
+		wxLog::SetActiveTarget(new wxLogStderr());
+	}
+
+	void TESTOpenDatabase()
+	{
+		TS_ASSERT(m_DB->DataBaseOpen(g_TestPathPRJ, g_TestPrj_CombioulaCorrect));
+		wxString myFalsePath = g_TestPathPRJ + _T("JJJ");
+		TS_ASSERT( m_DB->DataBaseOpen(myFalsePath, g_TestPrj_CombioulaExercice)==false);
+		TS_ASSERT( m_DB->DataBaseOpen(g_TestPathPRJ, g_TestPrj_CombioulaExercice));
+	}
+	
+};
 
 
 
