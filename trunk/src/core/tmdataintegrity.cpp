965afc37b5bb1855cb9e5b78b7a36f82ae8b2686
lucsch <lucsch@8d3dc7bd-f08f-3943-bf3e-8d26abd385ca>
2010-02-04 09:09:44 +0000

    class tmDataIntegrity is now fully working. Has to be added to ToolMap's export process now

--- trunk/src/core/tmdataintegrity.cpp
+++ trunk/src/core/tmdataintegrity.cpp
@@ -26,7 +26,7 @@ tmDataIntegrity::tmDataIntegrity(DataBaseTM * database) {
 	m_ErrorType = wxNOT_FOUND;
 	m_ErrorAAttrib = wxNOT_FOUND;
 	
-	m_QueryTemplateType = _T(" FROM %s AS mix WHERE mix.OBJECT_GEOM_ID NOT IN (SELECT OBJECT_ID FROM %s)");
+	m_QueryTemplateType = _T(" FROM %s WHERE OBJECT_GEOM_ID NOT IN (SELECT OBJECT_ID FROM %s)");
 	m_QueryTemplateAAttrib = _T(" FROM ") + TABLE_NAME_LAYER_AT +
 	_T("%d WHERE OBJECT_ID NOT IN (SELECT OBJECT_GEOM_ID FROM %s)");
 }
@@ -37,20 +37,20 @@ tmDataIntegrity::~tmDataIntegrity() {
 }
 
 
-bool tmDataIntegrity::CheckType(const TOC_GENERIC_NAME & layertype) {
+bool tmDataIntegrity::HasErrorType(const TOC_GENERIC_NAME & layertype) {
 	wxASSERT(m_pDB);
 	m_ErrorType = wxNOT_FOUND;
 	m_ErrorAAttrib = wxNOT_FOUND;
 	
 	// preparing queries
-	wxString myQueryType = _T("SELECT *");
+	wxString myQueryType = _T("SELECT * ");
 	myQueryType.Append(wxString::Format(m_QueryTemplateType,
 										TABLE_NAME_GIS_ATTRIBUTION[layertype].c_str(),
 										TABLE_NAME_GIS_GENERIC[layertype].c_str()));
 	
 	// running query (type)
 	if(m_pDB->DataBaseQuery(myQueryType)==false){
-		return false;
+		return true;
 	}
 	
 	if (m_pDB->DataBaseHasResults()==true) {
@@ -59,22 +59,22 @@ bool tmDataIntegrity::CheckType(const TOC_GENERIC_NAME & layertype) {
 	}
 	
 	if (m_ErrorType != wxNOT_FOUND) {
-		return false;
+		return true;
 	}
 	
-	return true;
+	return false;
 }
 
 
 
 
 
-bool tmDataIntegrity::CheckAAttrib(long layerindex, const TOC_GENERIC_NAME & layertype) {
+bool tmDataIntegrity::HasErrorAAttrib(long layerindex, const TOC_GENERIC_NAME & layertype) {
 	wxASSERT(m_pDB);
 	m_ErrorType = wxNOT_FOUND;
 	m_ErrorAAttrib = wxNOT_FOUND;
 
-	wxString myQueryAAttrib = _T("SELECT *");
+	wxString myQueryAAttrib = _T("SELECT * ");
 	myQueryAAttrib.Append(wxString::Format(m_QueryTemplateAAttrib,
 										   layerindex,
 										   TABLE_NAME_GIS_ATTRIBUTION[layertype].c_str()));
@@ -83,7 +83,7 @@ bool tmDataIntegrity::CheckAAttrib(long layerindex, const TOC_GENERIC_NAME & lay
 	// running query (aattrib)
 	if(m_pDB->DataBaseQuery(myQueryAAttrib)==false){
 		// advanced table may not exist
-		return true;
+		return false;
 	}
 	
 	if (m_pDB->DataBaseHasResults()==true) {
@@ -92,20 +92,50 @@ bool tmDataIntegrity::CheckAAttrib(long layerindex, const TOC_GENERIC_NAME & lay
 	}
 	
 	if (m_ErrorAAttrib != wxNOT_FOUND) {
-		return false;
+		return true;
 	}
 	
+	return false;
+	
+}
+
+
+bool tmDataIntegrity::CorrectType(const TOC_GENERIC_NAME & layertype) {
+	wxASSERT(m_pDB);
+	
+	// preparing queries
+	wxString myQueryType = _T("DELETE ");
+	myQueryType.Append(wxString::Format(m_QueryTemplateType,
+										TABLE_NAME_GIS_ATTRIBUTION[layertype].c_str(),
+										TABLE_NAME_GIS_GENERIC[layertype].c_str()));
+	// running query (type)
+	if(m_pDB->DataBaseQueryNoResults(myQueryType)==false){
+		return false;
+	}
+	m_ErrorType = wxNOT_FOUND;
 	return true;
 	
 }
 
-bool tmDataIntegrity::CorrectType(long layerindex, const TOC_GENERIC_NAME & layertype) {
-	return false;
-}
 
 bool tmDataIntegrity::CorrectAAttrib(long layerindex, const TOC_GENERIC_NAME & layertype) {
-	return false;
+	wxASSERT(m_pDB);
+
+	wxString myQueryAAttrib = _T("DELETE ");
+	myQueryAAttrib.Append(wxString::Format(m_QueryTemplateAAttrib,
+										   layerindex,
+										   TABLE_NAME_GIS_ATTRIBUTION[layertype].c_str()));
+	
+	
+	// running query (aattrib)
+	if(m_pDB->DataBaseQueryNoResults(myQueryAAttrib)==false){
+		// advanced table should exist here
+		return false;
+	}
+	m_ErrorAAttrib = wxNOT_FOUND;
+	return true;
 }
+
 
 wxString tmDataIntegrity::GetLastError() {
 	if (m_ErrorType != wxNOT_FOUND && m_ErrorAAttrib != wxNOT_FOUND) {
