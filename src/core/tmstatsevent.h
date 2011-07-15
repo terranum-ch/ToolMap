3f1d3c6f8dd53aeef34fd7d9fd6e6750d99c2331
lucsch <lucien.schreiber@terranum.ch>
2011-07-15 14:43:27 +0000

    Adding ctStatsData and ctStatsManager class (preliminary code)

---
+++ src/core/tmstatsevent.h
@@ -0,0 +1,33 @@
+/***************************************************************************
+ tmstatsevents.h
+ Definition of Statistics related events 
+ -------------------
+ copyright            : (C) 2011 CREALP Lucien Schreiber 
+ email                : lucien.schreiber at crealp dot vs dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _TMSTATS_EVENT_H
+#define _TMSTATS_EVENT_H
+
+
+#include "wx/wxprec.h"
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+
+
+DECLARE_EVENT_TYPE(tmEVT_STAT_CLICK, -1);
+DECLARE_EVENT_TYPE(tmEVT_STAT_ATTRIB, -1);
+DECLARE_EVENT_TYPE(tmEVT_STAT_INTERSECTION, -1);
+
+
+#endif
