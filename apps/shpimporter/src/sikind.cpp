d5d4fb15ad6087358dc851ad4a15e217b9d074ca
lucsch <lucien.schreiber@terranum.ch>
2012-12-21 14:26:02 +0000

    Adding preliminary code for shpimporter

---
+++ apps/shpimporter/src/sikind.cpp
@@ -0,0 +1,27 @@
+/***************************************************************************
+ sikind.cpp
+ -------------------
+ copyright            : (C) 2012 CREALP Lucien Schreiber 
+ email                : lucien.schreiber at crealp dot vs dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "sikind.h"
+
+siKind::siKind() {
+}
+
+siKind::~siKind() {
+}
+
+bool siKind::LoadFromTxt(const wxString & kindtxt) {
+}
+
