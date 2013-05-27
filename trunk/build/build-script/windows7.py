c3cd0721e995b279b2ce1f489a9c21f3f19e6fbc
lucsch <lucsch@8d3dc7bd-f08f-3943-bf3e-8d26abd385ca>
2013-05-27 11:47:34 +0000

    updating Windows build script for copying GDAL 1.10 DLL

--- trunk/build/build-script/windows7.py
+++ trunk/build/build-script/windows7.py
@@ -95,7 +95,7 @@ def gCreateInstaller(svnnumner):
 
 def DoPostBuildCommand():
     """copy DLL to directories"""
-    myDll = (os.path.join(gDirGdal,"bin","gdal19.dll"), #,os.path.join(gDirGeos,"bin","Debug", "geos.dll"),
+    myDll = (os.path.join(gDirGdal,"bin","gdal110.dll"), #,os.path.join(gDirGeos,"bin","Debug", "geos.dll"),
              os.path.join(gDirGeos,"src","geos_c.dll"),
              os.path.join(gDirSQL, "Embedded", "DLL", "release", "libmysqld.dll"),
              os.path.join(gDirCurl, "lib", "DLL-Debug","libcurld.dll"),
