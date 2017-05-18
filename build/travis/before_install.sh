8207c962105dbf828e780cf46028daf42db30f9d
Pascal Horton <pascal.horton@terranum.ch>
2017-05-18 16:51:54 +0200

    Fixing path for Travis.

--- build/travis/before_install.sh
+++ build/travis/before_install.sh
@@ -14,26 +14,28 @@ sudo apt-get install -y libnuma-dev
 
 # Build GDAL2
 if [ ! -d "${HOME}/.gdal" ]; then
-  cd(${HOME}/.gdal)
+  mkdir ${HOME}/.gdal
+  cd ${HOME}/.gdal
   wget -O gdal.tar.gz "http://download.osgeo.org/gdal/2.1.3/gdal-2.1.3.tar.gz"
   tar -xvzf gdal.tar.gz
   cd gdal-2.1.3
   ./configure --with-geos=/usr --with-static-proj4=/usr --with-sqlite3=yes --with-python=no --with-pg=no --with-grass=no --with-jasper=no --with-jpeg=internal --with-png=internal
   make
   sudo make install
-  cd(${TRAVIS_BUILD_DIR})
+  cd ${TRAVIS_BUILD_DIR}
 fi
 
 # Build wxPdfDocument
 if [ ! -d "${HOME}/.wxpdfdoc" ]; then
-  cd(${HOME}/.wxpdfdoc)
+  mkdir ${HOME}/.wxpdfdoc
+  cd ${HOME}/.wxpdfdoc
   wget -O wxpdfdoc.tar.gz "https://github.com/utelle/wxpdfdoc/releases/download/v0.9.5/wxpdfdoc-0.9.5.tar.gz"
   tar -xvzf wxpdfdoc.tar.gz
   cd wxpdfdoc-0.9.5
   ./configure --enable-unicode --disable-shared
   make
   sudo make install
-  cd(${TRAVIS_BUILD_DIR})
+  cd ${TRAVIS_BUILD_DIR}
 fi
 
 # CMake
