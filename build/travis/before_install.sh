9ebb6a68a00e7684cfd9a64f2a18c8132dc8c4c6
Pascal Horton <pascal.horton@terranum.ch>
2017-05-18 16:46:15 +0200

    Adding cache directories for built libraries.

--- build/travis/before_install.sh
+++ build/travis/before_install.sh
@@ -2,9 +2,6 @@
 
 # Install required libraries
 sudo apt-get -qq update
-sudo apt-get install -y build-essential
-sudo apt-get install -y gnome-devel
-sudo apt-get install -y libwxgtk3.0
 sudo apt-get install -y libwxgtk3.0-dev
 sudo apt-get install -y libwxgtk-media3.0-dev
 sudo apt-get install -y libwxgtk-webview3.0-dev
@@ -16,20 +13,28 @@ sudo apt-get install -y liblz4-dev
 sudo apt-get install -y libnuma-dev
 
 # Build GDAL2
-wget -O gdal.tar.gz "http://download.osgeo.org/gdal/2.1.3/gdal-2.1.3.tar.gz"
-tar -xvzf gdal.tar.gz
-cd gdal-2.1.3
-./configure --with-geos=/usr --with-static-proj4=/usr --with-sqlite3=yes --with-python=no --with-pg=no --with-grass=no --with-jasper=no --with-jpeg=internal --with-png=internal
-make
-sudo make install
+if [ ! -d "${HOME}/.gdal" ]; then
+  cd(${HOME}/.gdal)
+  wget -O gdal.tar.gz "http://download.osgeo.org/gdal/2.1.3/gdal-2.1.3.tar.gz"
+  tar -xvzf gdal.tar.gz
+  cd gdal-2.1.3
+  ./configure --with-geos=/usr --with-static-proj4=/usr --with-sqlite3=yes --with-python=no --with-pg=no --with-grass=no --with-jasper=no --with-jpeg=internal --with-png=internal
+  make
+  sudo make install
+  cd(${TRAVIS_BUILD_DIR})
+fi
 
 # Build wxPdfDocument
-wget -O wxpdfdoc.tar.gz "https://github.com/utelle/wxpdfdoc/releases/download/v0.9.5/wxpdfdoc-0.9.5.tar.gz"
-tar -xvzf wxpdfdoc.tar.gz
-cd wxpdfdoc-0.9.5
-./configure --enable-unicode --disable-shared
-make
-sudo make install
+if [ ! -d "${HOME}/.wxpdfdoc" ]; then
+  cd(${HOME}/.wxpdfdoc)
+  wget -O wxpdfdoc.tar.gz "https://github.com/utelle/wxpdfdoc/releases/download/v0.9.5/wxpdfdoc-0.9.5.tar.gz"
+  tar -xvzf wxpdfdoc.tar.gz
+  cd wxpdfdoc-0.9.5
+  ./configure --enable-unicode --disable-shared
+  make
+  sudo make install
+  cd(${TRAVIS_BUILD_DIR})
+fi
 
 # CMake
 mkdir bin
