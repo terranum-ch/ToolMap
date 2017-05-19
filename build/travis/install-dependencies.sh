ecf77a86732f4a31a65843f4f5843f15bcbf7a24
Pascal Horton <pascal.horton@giub.unibe.ch>
2017-05-19 10:23:31 +0200

    Update install-dependencies.sh

--- build/travis/install-dependencies.sh
+++ build/travis/install-dependencies.sh
@@ -14,7 +14,7 @@ if [ ! "$(ls -A ${HOME}/.gdal)" ]; then
   wget -O gdal.tar.gz "http://download.osgeo.org/gdal/2.1.3/gdal-2.1.3.tar.gz"
   tar -xzf gdal.tar.gz
   cd gdal-2.1.3
-  ./configure --prefix=${HOME}/.gdal --with-geos=/usr --with-static-proj4=/usr --with-sqlite3=yes --with-python=no --with-pg=no --with-grass=no --with-jasper=no --with-jpeg=internal --with-png=internal --silent
+  ./configure --prefix=${HOME}/.gdal --with-geos=/usr --with-static-proj4=/usr --with-sqlite3=yes --with-python=no --with-pg=no --with-grass=no --with-jasper=no --with-jpeg=internal --with-png=internal --silent --enable-silent-rules
   make -s
   make install
   cd ..
