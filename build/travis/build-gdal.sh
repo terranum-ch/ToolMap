08e5c27490b35e23d46581e9206fb403066e6302
pascalhorton <pascal.horton@giub.unibe.ch>
2017-05-19 13:34:26 +0200

    Starting Travis build to cache the libraries.

--- build/travis/build-gdal.sh
+++ build/travis/build-gdal.sh
@@ -2,7 +2,7 @@
 
 # Build GDAL2
 if [ ! "$(ls -A ${HOME}/.gdal)" ]; then
-  wget -O gdal.tar.gz "http://download.osgeo.org/gdal/2.1.3/gdal-2.1.3.tar.gz" > /dev/null
+  wget -q -O gdal.tar.gz "http://download.osgeo.org/gdal/2.1.3/gdal-2.1.3.tar.gz" > /dev/null
   tar -xzf gdal.tar.gz
   cd gdal-2.1.3
   ./configure --prefix=${HOME}/.gdal --with-geos=/usr --with-static-proj4=/usr --with-sqlite3=yes --with-python=no --with-pg=no --with-grass=no --with-jasper=no --with-jpeg=internal --with-png=internal --silent
--- build/travis/build-wxpdfdoc.sh
+++ build/travis/build-wxpdfdoc.sh
@@ -2,7 +2,7 @@
 
 # Build wxPdfDocument
 if [ ! "$(ls -A ${HOME}/.wxpdfdoc)" ]; then
-  wget -O wxpdfdoc.tar.gz "https://github.com/utelle/wxpdfdoc/releases/download/v0.9.5/wxpdfdoc-0.9.5.tar.gz" > /dev/null
+  wget -q -O wxpdfdoc.tar.gz "https://github.com/utelle/wxpdfdoc/releases/download/v0.9.5/wxpdfdoc-0.9.5.tar.gz" > /dev/null
   tar -xzf wxpdfdoc.tar.gz
   cd wxpdfdoc-0.9.5
   ./configure --prefix=${HOME}/.wxpdfdoc --enable-unicode --disable-shared --silent
