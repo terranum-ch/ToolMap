c3274a4ecb21efc81a1be3b59a8e6d7917e2a92d
pascalhorton <pascal.horton@giub.unibe.ch>
2017-05-19 17:14:23 +0200

    Buidling wxWebView on Travis (#367)

--- build/travis/build-wxwidgets.sh
+++ build/travis/build-wxwidgets.sh
@@ -5,7 +5,7 @@ if [ ! "$(ls -A ${HOME}/.wxwidgets)" ]; then
   wget -q -O wxwidgets.tar.bz2 "https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.0/wxWidgets-3.1.0.tar.bz2" > /dev/null
   tar -xjf wxwidgets.tar.bz2
   cd wxWidgets-3.1.0
-  ./configure --prefix=${HOME}/.wxwidgets --enable-unicode --disable-shared --enable-mediactrl=no --silent
+  ./configure --prefix=${HOME}/.wxwidgets --enable-unicode --disable-shared --enable-mediactrl=no --enable-webview --enable-webview-webkit --silent
   make -j2 > /dev/null
   make install > /dev/null
   cd ..
