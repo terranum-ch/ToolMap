6330e5a8d03c35d0464ff35bc65117d787f53576
Pascal Horton <pascal.horton@giub.unibe.ch>
2017-05-19 09:52:42 +0200

    Trying to reduce verbosity for Travis (#367).

--- build/travis/install-dependencies.sh
+++ build/travis/install-dependencies.sh
@@ -12,10 +12,10 @@ sudo apt-get install -y libnuma-dev
 # Build GDAL2
 if [ ! "$(ls -A ${HOME}/.gdal)" ]; then
   wget -O gdal.tar.gz "http://download.osgeo.org/gdal/2.1.3/gdal-2.1.3.tar.gz"
-  tar -xvzf gdal.tar.gz
+  tar -xzf gdal.tar.gz
   cd gdal-2.1.3
   ./configure --prefix=${HOME}/.gdal --with-geos=/usr --with-static-proj4=/usr --with-sqlite3=yes --with-python=no --with-pg=no --with-grass=no --with-jasper=no --with-jpeg=internal --with-png=internal --silent
-  make --silent
+  make -s
   make install
   cd ..
 else 
@@ -25,10 +25,10 @@ fi
 # Build wxWidgets
 if [ ! "$(ls -A ${HOME}/.wxwidgets)" ]; then
   wget -O wxwidgets.tar.bz2 "https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.0/wxWidgets-3.1.0.tar.bz2"
-  tar -xvjf wxwidgets.tar.bz2
+  tar -xjf wxwidgets.tar.bz2
   cd wxWidgets-3.1.0
   ./configure --prefix=${HOME}/.wxwidgets --enable-unicode --disable-shared --enable-mediactrl=no --silent
-  make --silent
+  make -s
   make install
   cd ..
 else 
@@ -38,10 +38,10 @@ fi
 # Build wxPdfDocument
 if [ ! "$(ls -A ${HOME}/.wxpdfdoc)" ]; then
   wget -O wxpdfdoc.tar.gz "https://github.com/utelle/wxpdfdoc/releases/download/v0.9.5/wxpdfdoc-0.9.5.tar.gz"
-  tar -xvzf wxpdfdoc.tar.gz
+  tar -xzf wxpdfdoc.tar.gz
   cd wxpdfdoc-0.9.5
   ./configure --prefix=${HOME}/.wxpdfdoc --enable-unicode --disable-shared --silent
-  make --silent
+  make -s
   make install
   cd ..
 else 
