f69aed4df5f82e77f806d49387c4f77709453994
pascalhorton <pascal.horton@giub.unibe.ch>
2017-05-19 16:57:49 +0200

    Fixing a wxpdfdoc build issue for Travis.

--- build/travis/build-wxpdfdoc.sh
+++ build/travis/build-wxpdfdoc.sh
@@ -5,7 +5,7 @@ if [ ! "$(ls -A ${HOME}/.wxpdfdoc)" ]; then
   wget -q -O wxpdfdoc.tar.gz "https://github.com/utelle/wxpdfdoc/releases/download/v0.9.5/wxpdfdoc-0.9.5.tar.gz" > /dev/null
   tar -xzf wxpdfdoc.tar.gz
   cd wxpdfdoc-0.9.5
-  ./configure --prefix=${HOME}/.wxpdfdoc --with-wxdir=${HOME}/.wxwidgets --enable-unicode --disable-shared --silent
+  ./configure --prefix=${HOME}/.wxpdfdoc --with-wx-prefix=${HOME}/.wxwidgets --enable-unicode --disable-shared --silent
   make -j2 > /dev/null
   make install > /dev/null
   cd ..
