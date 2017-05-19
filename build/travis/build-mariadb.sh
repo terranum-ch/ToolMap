aecfadc47100b2026b15ce9089a2eb5bd744aeec
pascalhorton <pascal.horton@giub.unibe.ch>
2017-05-19 14:14:48 +0200

    Trying to fix MariaDB on Travis (see #367)

--- build/travis/build-mariadb.sh
+++ build/travis/build-mariadb.sh
@@ -5,7 +5,7 @@ if [ ! "$(ls -A ${HOME}/.mariadb)" ]; then
   wget -q -O mariadb.tar.gz "https://github.com/MariaDB/server/archive/mariadb-10.1.23.tar.gz" > /dev/null
   tar -xzf mariadb.tar.gz
   cd server-mariadb-10.1.23
-  cmake . -DCMAKE_INSTALL_PREFIX=${HOME}/.mariadb -DPLUGIN_TOKUDB=NO -DWITH_EMBEDDED_SERVER=1 -DDISABLE_SHARED=ON > /dev/null
+  cmake . -DCMAKE_INSTALL_PREFIX=${HOME}/.mariadb -DPLUGIN_TOKUDB=NO -DWITH_EMBEDDED_SERVER=1 > /dev/null
   make > /dev/null
   make install > /dev/null
   cd ..
