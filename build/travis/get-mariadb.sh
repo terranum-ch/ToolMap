4124862dea27fb6253aec2f5ac500ac6de94b8af
Pascal Horton <pascal.horton@giub.unibe.ch>
2017-05-19 21:57:24 +0200

    Update get-mariadb.sh

--- build/travis/get-mariadb.sh
+++ build/travis/get-mariadb.sh
@@ -6,4 +6,4 @@ sudo apt-get install python-software-properties
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
 sudo add-apt-repository 'deb [arch=amd64,i386] http://ftp.heanet.ie/mirrors/mariadb/repo/10.1/ubuntu precise main'
 sudo apt-get update
-sudo apt-get install libmariadbd-dev libmysqlclient-dev
+sudo apt-get install libmariadbd-dev mariadb-server
