ba273661135a9becde8f9c2cb8385c232ab31258
Pascal Horton <pascal.horton@giub.unibe.ch>
2017-05-19 09:30:43 +0200

    Reducing verbosity of libraries build.
    Working on Travis CI (see #367)

--- build/travis/before_install.sh
+++ build/travis/before_install.sh
@@ -14,8 +14,8 @@ if [ ! "$(ls -A ${HOME}/.gdal)" ]; then
   wget -O gdal.tar.gz "http://download.osgeo.org/gdal/2.1.3/gdal-2.1.3.tar.gz"
   tar -xvzf gdal.tar.gz
   cd gdal-2.1.3
-  ./configure --prefix=${HOME}/.gdal --with-geos=/usr --with-static-proj4=/usr --with-sqlite3=yes --with-python=no --with-pg=no --with-grass=no --with-jasper=no --with-jpeg=internal --with-png=internal
-  make
+  ./configure --prefix=${HOME}/.gdal --with-geos=/usr --with-static-proj4=/usr --with-sqlite3=yes --with-python=no --with-pg=no --with-grass=no --with-jasper=no --with-jpeg=internal --with-png=internal --silent
+  make --silent
   make install
   cd ..
 else 
@@ -27,8 +27,8 @@ if [ ! "$(ls -A ${HOME}/.wxwidgets)" ]; then
   wget -O wxwidgets.tar.bz2 "https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.0/wxWidgets-3.1.0.tar.bz2"
   tar -xvjf wxwidgets.tar.bz2
   cd wxWidgets-3.1.0
-  ./configure --prefix=${HOME}/.wxwidgets --enable-unicode --disable-shared --enable-mediactrl=no
-  make
+  ./configure --prefix=${HOME}/.wxwidgets --enable-unicode --disable-shared --enable-mediactrl=no --silent
+  make --silent
   make install
   cd ..
 else 
@@ -40,8 +40,8 @@ if [ ! "$(ls -A ${HOME}/.wxpdfdoc)" ]; then
   wget -O wxpdfdoc.tar.gz "https://github.com/utelle/wxpdfdoc/releases/download/v0.9.5/wxpdfdoc-0.9.5.tar.gz"
   tar -xvzf wxpdfdoc.tar.gz
   cd wxpdfdoc-0.9.5
-  ./configure --prefix=${HOME}/.wxpdfdoc --enable-unicode --disable-shared
-  make
+  ./configure --prefix=${HOME}/.wxpdfdoc --enable-unicode --disable-shared --silent
+  make --silent
   make install
   cd ..
 else 
