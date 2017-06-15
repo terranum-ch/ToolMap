2d9e08c53c707926cba847f5e9902368f6b2dfa5
Lucien Schreiber <lucien.schreiber@terranum.ch>
2017-06-16 00:02:39 +0200

    Compilation is now working for Windows

--- build/cmake/findMySQLEmbedded.cmake
+++ build/cmake/findMySQLEmbedded.cmake
@@ -17,112 +17,59 @@ IF(NOT MYSQL_MAIN_DIR)
           /usr/include/mysql)
 ENDIF(NOT MYSQL_MAIN_DIR)
 
-# if mysql is installed in a normal directory
-IF (MYSQL_INCLUDE_DIR)
-    MESSAGE ("MYSQL Include dir defined")
-    #SET(MYSQL_NAMES mysqld)
-    #FIND_LIBRARY(MYSQL_LIBRARY
-      #NAMES ${MYSQL_NAMES}
-      #PATHS /usr/lib /usr/local/lib /usr/lib/mysql
-    #)
 
-    #IF (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
-      #	SET(MYSQL_FOUND TRUE)
-      #	SET( MYSQL_LIBRARIES ${MYSQL_LIBRARY} )
-    #	MESSAGE("MySQL found and linked ${MYSQL_LIBRARY}")
-    #	LINK_LIBRARIES(${MYSQL_LIBRARY})
-    #ELSE (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
-      #	SET(MYSQL_FOUND FALSE)
-      #	SET( MYSQL_LIBRARIES )
-    #ENDIF (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
-
-    #IF (MYSQL_FOUND)
-      #	IF (NOT MYSQL_FIND_QUIETLY)
-        #	MESSAGE(STATUS "Found MySQL: ${MYSQL_LIBRARY}")
-      #	ENDIF (NOT MYSQL_FIND_QUIETLY)
-    #ELSE (MYSQL_FOUND)
-      #	IF (MYSQL_FIND_REQUIRED)
-        #	MESSAGE(STATUS "Looked for MySQL libraries named ${MYSQL_NAMES}.")
-        #	MESSAGE(FATAL_ERROR "Could NOT find MySQL library")
-      #	ENDIF (MYSQL_FIND_REQUIRED)
-    #ENDIF (MYSQL_FOUND)
-
-    # EXEC_PROGRAM("mysql_config"
-    #     ARGS "--libmysqld-libs"
-    #     OUTPUT_VARIABLE MYSQL_CONFIG_LIBS)
-    #     MESSAGE("DBG: ${MYSQL_CONFIG_LIBS}")
-        
-    #     EXEC_PROGRAM("mysql_config"
-    #     ARGS "--cflags"
-    #     OUTPUT_VARIABLE MYSQL_CONFIG_CXFLAGS)
-    #     MESSAGE("DBG: ${MYSQL_CONFIG_CXFLAGS}")
-
-# if mysql is not installed in a normal
-#directory, specify the main directory
-ELSE (MYSQL_INCLUDE_DIR)
-    SET (MYSQL_MAIN_DIR CACHE PATH "Path to the main MySQL directory")
+# SET (MYSQL_MAIN_DIR CACHE PATH "Path to the main MySQL directory")    
+IF (WIN32)        
+    # search include directory based on main path
+    FIND_PATH(MYSQL_INCLUDE_DIR  mysql.h 
+        ${MYSQL_MAIN_DIR}
+        ${MYSQL_MAIN_DIR}/include)  
     
-    IF (WIN32)
-        
-        # search include directory based on main path
-        FIND_PATH(MYSQL_INCLUDE_DIR  mysql.h 
-            ${MYSQL_MAIN_DIR}
-            ${MYSQL_MAIN_DIR}/include)  
+    # find library
+    FIND_LIBRARY(MYSQL_LIBRARY_EMBEDDED_DEBUG 
+        NAMES libmysqld
+        PATHS
+    "${MYSQL_MAIN_DIR}/Embedded/DLL/debug"
+    "${MYSQL_MAIN_DIR}/lib/debug"
+    DOC "MySQL embedded debug library" )
+  
+    FIND_LIBRARY(MYSQL_LIBRARY_EMBEDDED 
+        NAMES libmysqld
+        PATHS
+    "${MYSQL_MAIN_DIR}/Embedded/DLL/release"
+    "${MYSQL_MAIN_DIR}/lib"
+    DOC "MySQL embedded library" )        
+    
+    # SET(MYSQL_CONFIG_LIBS ${MYSQL_CONFIG_LIBS}
+    #    debug ${MYSQL_LIBRARY_EMBEDDED_DEBUG}  
+    #    optimized ${MYSQL_LIBRARY_EMBEDDED})
+    SET(MYSQL_CONFIG_LIBS ${MYSQL_LIBRARY_EMBEDDED} CACHE FILEPATH "MySQL Libraries for linking")
+    MESSAGE("---------------------------------- ${MYSQL_CONFIG_LIBS}")
+    # path to libs
+    # IF(CMAKE_BUILD_TYPE STREQUAL "Debug")
+    #    SET(MYSQL_CONFIG_LIBS "${MYSQL_MAIN_DIR}/Embedded/DLL/debug/libmysqld.lib" )
+    #  ENDIF(CMAKE_BUILD_TYPE STREQUAL "Debug")        
+    #  IF(CMAKE_BUILD_TYPE STREQUAL "Release")
+    #    SET(MYSQL_CONFIG_LIBS "${MYSQL_MAIN_DIR}/Embedded/DLL/release/libmysqld.lib" )
+          # ENDIF(CMAKE_BUILD_TYPE STREQUAL "Release")                
 
-        # find library
-        FIND_LIBRARY(MYSQL_LIBRARY_EMBEDDED_DEBUG 
-            NAMES libmysqld
-            PATHS
-        "${MYSQL_MAIN_DIR}/Embedded/DLL/debug"
-        "${MYSQL_MAIN_DIR}/lib/debug"
-        DOC "MySQL embedded debug library" )
-
-        FIND_LIBRARY(MYSQL_LIBRARY_EMBEDDED 
-            NAMES libmysqld
-            PATHS
-        "${MYSQL_MAIN_DIR}/Embedded/DLL/release"
-        "${MYSQL_MAIN_DIR}/lib"
-        DOC "MySQL embedded library" )
-        
-        # SET(MYSQL_CONFIG_LIBS ${MYSQL_CONFIG_LIBS}
-        #    debug ${MYSQL_LIBRARY_EMBEDDED_DEBUG}  
-        #    optimized ${MYSQL_LIBRARY_EMBEDDED})
-        SET(MYSQL_CONFIG_LIBS ${MYSQL_LIBRARY_EMBEDDED})
-
-             # path to libs
-            # IF(CMAKE_BUILD_TYPE STREQUAL "Debug")
-            #    SET(MYSQL_CONFIG_LIBS "${MYSQL_MAIN_DIR}/Embedded/DLL/debug/libmysqld.lib" )
-            #  ENDIF(CMAKE_BUILD_TYPE STREQUAL "Debug")
-        
-            #  IF(CMAKE_BUILD_TYPE STREQUAL "Release")
-            #    SET(MYSQL_CONFIG_LIBS "${MYSQL_MAIN_DIR}/Embedded/DLL/release/libmysqld.lib" )
-                  # ENDIF(CMAKE_BUILD_TYPE STREQUAL "Release")
-        
-        
-
-    ELSE (WIN32)
-        SET(MYSQL_INCLUDE_DIR "${MYSQL_MAIN_DIR}/include/")
-        #MESSAGE ("MYSLQ_INCLUDE_DIR = ${MYSQL_INCLUDE_DIR}")
-        
-        SET(MYSQL_CONFIG_EXECUTABLE "${MYSQL_MAIN_DIR}/bin/mysql_config")
-        
-        SET(MYSQL_CONFIG_ARGS_LIBS "--libmysqld-libs")
-        SET(MYSQL_CONFIG_ARGS_CXFLAGS "--cflags")
-        
-        EXEC_PROGRAM(${MYSQL_CONFIG_EXECUTABLE}
-        ARGS ${MYSQL_CONFIG_ARGS_LIBS}
-        OUTPUT_VARIABLE MYSQL_CONFIG_LIBS)
-        MESSAGE("DBG: ${MYSQL_CONFIG_LIBS}")
-        
-        EXEC_PROGRAM(${MYSQL_CONFIG_EXECUTABLE}
-        ARGS ${MYSQL_CONFIG_ARGS_CXFLAGS}
-        OUTPUT_VARIABLE MYSQL_CONFIG_CXFLAGS)
-        MESSAGE("DBG: ${MYSQL_CONFIG_CXFLAGS}")
-        
-
-    ENDIF(WIN32)
-
-ENDIF (MYSQL_INCLUDE_DIR)
+ELSE (WIN32)
+    SET(MYSQL_INCLUDE_DIR "${MYSQL_MAIN_DIR}/include/")
+    #MESSAGE ("MYSLQ_INCLUDE_DIR = ${MYSQL_INCLUDE_DIR}")        
+    SET(MYSQL_CONFIG_EXECUTABLE "${MYSQL_MAIN_DIR}/bin/mysql_config")        
+    SET(MYSQL_CONFIG_ARGS_LIBS "--libmysqld-libs")
+    SET(MYSQL_CONFIG_ARGS_CXFLAGS "--cflags")        
+    
+    EXEC_PROGRAM(${MYSQL_CONFIG_EXECUTABLE}
+      ARGS ${MYSQL_CONFIG_ARGS_LIBS}
+      OUTPUT_VARIABLE MYSQL_CONFIG_LIBS)
+      MESSAGE("DBG: ${MYSQL_CONFIG_LIBS}")        
+    
+    EXEC_PROGRAM(${MYSQL_CONFIG_EXECUTABLE}
+      ARGS ${MYSQL_CONFIG_ARGS_CXFLAGS}
+      OUTPUT_VARIABLE MYSQL_CONFIG_CXFLAGS)
+      MESSAGE("DBG: ${MYSQL_CONFIG_CXFLAGS}")        
+ENDIF(WIN32)
 
 
 
@@ -166,8 +113,9 @@ MARK_AS_ADVANCED(
   ###IF(WXWINDOWS_LINK_DIRECTORIES)
   ####  LINK_DIRECTORIES(${WXWINDOWS_LINK_DIRECTORIES})
   ####ENDIF(WXWINDOWS_LINK_DIRECTORIES)
-  
+  MESSAGE("TEST TEST !!!!!!!!!!!!!!!!!!! ${MYSQL_CONFIG_LIBS}")
   IF(MYSQL_CONFIG_LIBS)
+	MESSAGE("!!!!!!!!!!!!!!!!!!! ${MYSQL_CONFIG_LIBS}")
     LINK_LIBRARIES(${MYSQL_CONFIG_LIBS})
   ENDIF(MYSQL_CONFIG_LIBS)
   
