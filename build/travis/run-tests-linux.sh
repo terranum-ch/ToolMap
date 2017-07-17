d13596dd87976bb2d8a2d71fece478483e9f1902
pascalhorton <pascal.horton@giub.unibe.ch>
2017-07-17 11:47:49 +0200

    Working on multi-platform CI on Travis
    See issue #367.

--- build/travis/run-tests-linux.sh
+++ build/travis/run-tests-linux.sh
@@ -4,5 +4,3 @@ mkdir bin
 cd bin
 cmake -DSEARCH_GDAL_PATH=${HOME}/.gdal -DSEARCH_GEOS_PATH=/usr -DMYSQL_MAIN_DIR=${HOME}/.mysql -DSEARCH_WXPDFDOCUMENT_PATH=${HOME}/.wxpdfdoc -DSEARCH_GEOS=1 -DSEARCH_GDAL=1 -DUSE_UNITTEST=1 -DCXXTEST_INCLUDE_DIR=/usr -DCXXTEST_PYTHON_TESTGEN_EXECUTABLE=/usr/bin/cxxtestgen ..
 make -j$(nproc)
-cd test
-ctest -V
