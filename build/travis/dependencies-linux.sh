1d50f2bc70c23954b0f86250282ef81a9a9b4219
pascalhorton <pascal.horton@giub.unibe.ch>
2017-07-17 12:32:31 +0200

    Re-building wxWidgets on Linux.

--- build/travis/dependencies-linux.sh
+++ build/travis/dependencies-linux.sh
@@ -10,7 +10,5 @@ sudo apt-get install -y libwrap0-dev
 sudo apt-get install -y cxxtest
 
 # wxWidgets
-sudo apt-key adv --fetch-keys http://repos.codelite.org/CodeLite.asc
-sudo apt-add-repository 'deb http://repos.codelite.org/wx3.1/ubuntu/ trusty universe'
-sudo apt-get update
-sudo apt-get install libwxbase3.1-0-unofficial libwxbase3.1-dev libwxgtk3.1-0-unofficial libwxgtk3.1-dev wx3.1-headers wx-common
+chmod +x build/travis/build-wxwidgets.sh
+build/travis/build-wxwidgets.sh
