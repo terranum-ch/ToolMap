89c8d5c225787678822db948d56d91441c4a112d
pascalhorton <pascal.horton@giub.unibe.ch>
2017-07-17 15:04:46 +0200

    Restoring path to wxWidgets when building wxPdfDoc on Linux.

--- build/travis/build-wxpdfdoc.sh
+++ build/travis/build-wxpdfdoc.sh
@@ -5,10 +5,15 @@ if [ ! "$(ls -A ${HOME}/.wxpdfdoc)" ]; then
   wget -q -O wxpdfdoc.tar.gz "https://github.com/utelle/wxpdfdoc/releases/download/v0.9.5/wxpdfdoc-0.9.5.tar.gz" > /dev/null
   tar -xzf wxpdfdoc.tar.gz
   cd wxpdfdoc-0.9.5
-  ./configure --prefix=${HOME}/.wxpdfdoc --enable-unicode --disable-shared --silent
+  if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
+    ./configure --prefix=${HOME}/.wxpdfdoc --with-wx-prefix=${HOME}/.wxwidgets --enable-unicode --disable-shared --silent
+  else
+    ./configure --prefix=${HOME}/.wxpdfdoc --enable-unicode --disable-shared --silent
+  fi
   make -j4 > /dev/null
   make install > /dev/null
   cd ..
+  printf 'wxPdfDoc was built.\n'
 else 
   printf 'wxPdfDoc will not be built (%s/.wxpdfdoc not empty).\n' "$HOME"
 fi
