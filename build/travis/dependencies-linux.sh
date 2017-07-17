a67c06d6abf26f910f5a07db5062fe4de27cdf69
Pascal Horton <pascal.horton@giub.unibe.ch>
2017-07-17 09:23:58 +0200

    OSX build CI under work.
    See issue #367

---
+++ build/travis/dependencies-linux.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+sudo apt-get -qq update
+sudo apt-get install -y libproj-dev
+sudo apt-get install -y libgeos-dev
+sudo apt-get install -y libaio-dev
+sudo apt-get install -y libnuma-dev
+sudo apt-get install -y libwebkitgtk-dev
+sudo apt-get install -y libwrap0-dev
+sudo apt-get install -y cxxtest
---
+++ build/travis/dependencies-osx.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+brew install proj
+brew install geos
+brew install cxxtest
