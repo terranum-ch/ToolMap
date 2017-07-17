ee4d92987b572c7c98476515eb9e88e78c4c644e
pascalhorton <pascal.horton@giub.unibe.ch>
2017-07-17 16:12:57 +0200

    Trying to fix wxpdfdoc build on Linux.

--- build/travis/build-wxpdfdoc.sh
+++ build/travis/build-wxpdfdoc.sh
@@ -5,7 +5,7 @@ if [ ! "$(ls -A ${HOME}/.wxpdfdoc)" ]; then
   wget -q -O wxpdfdoc.tar.gz "https://github.com/utelle/wxpdfdoc/releases/download/v0.9.5/wxpdfdoc-0.9.5.tar.gz" > /dev/null
   tar -xzf wxpdfdoc.tar.gz
   cd wxpdfdoc-0.9.5
-  if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
+  if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     ./configure --prefix=${HOME}/.wxpdfdoc --with-wx-prefix=${HOME}/.wxwidgets --enable-unicode --disable-shared --silent
   else
     ./configure --prefix=${HOME}/.wxpdfdoc --enable-unicode --disable-shared --silent
