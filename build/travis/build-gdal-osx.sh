ec293656cf72531e34f2eb882ea7f55ee7d08691
Pascal Horton <pascal.horton@terranum.ch>
2017-09-02 08:07:45 +0200

    Debugging Travis osx build.
    See issue #367.

---
+++ build/travis/build-gdal-osx.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+REBUILD_GDAL=true
+
+# Build GDAL2
+if [ ! "$(ls -A ${HOME}/.gdal)" ] || [ "$REBUILD_GDAL" = true ]; then
+  if [ "$REBUILD_GDAL" = true ]; then
+    rm -rf ${HOME}/.gdal
+  fi
+  wget -q -O gdal.tar.gz "http://download.osgeo.org/gdal/2.1.3/gdal-2.1.3.tar.gz" > /dev/null
+  tar -xzf gdal.tar.gz
+  cd gdal-2.1.3
+  ./configure --prefix=${HOME}/.gdal --with-geos=/usr/local/bin/geos-config --with-static-proj4=/usr --with-sqlite3=yes --with-python=no --with-pg=no --with-grass=no --with-jasper=no --with-jpeg=internal --with-png=internal --silent
+  make -j4 > /dev/null
+  make install > /dev/null
+  cd ..
+else 
+  printf 'GDAL will not be built (%s/.gdal not empty).\n' "$HOME"
+fi
--- build/travis/dependencies-osx.sh
+++ build/travis/dependencies-osx.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 brew install proj
-brew install geos
+brew upgrade geos
 
 wget -q -O cxxtest.tar.gz "https://github.com/CxxTest/cxxtest/archive/4.3.tar.gz"
 tar -xzf cxxtest.tar.gz -C ${HOME}
@@ -9,8 +9,8 @@ tar -xzf cxxtest.tar.gz -C ${HOME}
 # Build libraries
 chmod +x build/travis/build-wxwidgets-osx.sh
 build/travis/build-wxwidgets-osx.sh
-chmod +x build/travis/build-gdal.sh
-build/travis/build-gdal.sh
+chmod +x build/travis/build-gdal-osx.sh
+build/travis/build-gdal-osx.sh
 chmod +x build/travis/build-wxpdfdoc.sh
 build/travis/build-wxpdfdoc.sh
 chmod +x build/travis/build-mysql.sh
