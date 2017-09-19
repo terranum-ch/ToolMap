cd2f8ecfc01fcc933821d600468886c922f00ac6
Unknown <lucien.schreiber@terranum.ch>
2017-09-19 14:18:53 +0200

    see #351 tmGisDataRasterWeb code cleaning

--- src/gis/tmgisdatarasterweb.h
+++ src/gis/tmgisdatarasterweb.h
@@ -30,29 +30,14 @@ class tmGISDataRasterWeb : public tmGISDataRaster
 private:
     tmRealRect m_FilterCoordLocal;
     tmRealRect m_FilterCoordWeb;
-    tmWebFrame * m_WebFrameRef;
-    static const double m_AvaillableResolutions [];
-    double _GetClosestAvaillableResolution(tmRealRect bounds, double resolution);
-    double _GetResolution (tmRealRect bounds);
 
 public:
     tmGISDataRasterWeb();
     ~tmGISDataRasterWeb();
     
-    // void SetWebFrameRef (tmWebFrame * webframe){m_WebFrameRef = webframe;}
-    // tmWebFrame * GetWebFrameRef() {return m_WebFrameRef;}
-    
     virtual wxString GetDataSizeAsHtml (int iPrecision = 2);
-	// virtual bool Open (const wxString & filename, bool bReadWrite = TRUE);
-    
-    // virtual tmRealRect GetMinimalBoundingRectangle();
     virtual bool SetSpatialFilter (tmRealRect filter, int type);
-//	virtual bool IsImageInsideVisibleArea ();
     virtual tmRealRect GetImageClipedCoordinates () {return m_FilterCoordLocal;}
-	//virtual CPLErr GetImageData(unsigned char **imgbuf, unsigned int   *imglen,
-    //                            unsigned char **maskbuf, unsigned int   *masklen,
-    //                            wxSize imgSize);
-
 };
 
 #endif
