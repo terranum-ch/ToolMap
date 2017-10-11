1788736a93fc8dadc165e04980014f1bb0111ea4
Pascal Horton <pascal.horton@terranum.ch>
2017-10-11 23:44:30 +0200

    Close #394 by updating CMake version
    The updated version of Visual Studio was too recent for the installed
    version of CMake.

--- build/appveyor/install.ps1
+++ build/appveyor/install.ps1
@@ -11,7 +11,7 @@ $REBUILD_WX=$false
 $REBUILD_WXPDF=$false
 $REBUILD_PROJ=$false
 $REBUILD_GEOS=$false
-$REBUILD_GDAL=$true
+$REBUILD_GDAL=$false
 $REBUILD_MYSQL=$false
 $REBUILD_CURL=$false
 
@@ -39,7 +39,7 @@ if(!(Test-Path -Path $TMP_DIR)) {
 if(!(Test-Path -Path "$LIB_DIR\cmake")) {
   Write-Host "`nInstalling CMake" -ForegroundColor Yellow
   cd $TMP_DIR
-  $CMAKE_URL="https://cmake.org/files/v3.8/cmake-3.8.0-win64-x64.zip"
+  $CMAKE_URL="https://cmake.org/files/v3.9/cmake-3.9.4-win64-x64.zip"
   if ($ON_APPVEYOR) {
     appveyor DownloadFile $CMAKE_URL -FileName cmake.zip > $null
   } else {
