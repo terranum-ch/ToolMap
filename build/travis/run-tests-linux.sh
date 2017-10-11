8b998f9ad81b7ba3c23838a87ce035a7e7d61c51
Pascal Horton <pascal.horton@terranum.ch>
2017-10-12 00:07:28 +0200

    Limiting the use of AppImage only when specified.

--- build/travis/run-tests-linux.sh
+++ build/travis/run-tests-linux.sh
@@ -6,5 +6,5 @@ wget "https://github.com/AppImage/AppImageKit/releases/download/9/appimagetool-x
 wget "https://github.com/AppImage/AppImageKit/releases/download/9/AppRun-x86_64"
 chmod a+x appimagetool-x86_64.AppImage
 chmod a+x AppRun-x86_64
-cmake -DSEARCH_GDAL_PATH=${HOME}/.gdal -DSEARCH_GEOS_PATH=/usr -DMYSQL_MAIN_DIR=${HOME}/.mysql -DSEARCH_WXPDFDOCUMENT_PATH=${HOME}/.wxpdfdoc -DSEARCH_GEOS=1 -DSEARCH_GDAL=1 -DUSE_UNITTEST=1 -DCXXTEST_INCLUDE_DIR=/usr -DCXXTEST_PYTHON_TESTGEN_EXECUTABLE=/usr/bin/cxxtestgen ..
+cmake -DSEARCH_GDAL_PATH=${HOME}/.gdal -DSEARCH_GEOS_PATH=/usr -DMYSQL_MAIN_DIR=${HOME}/.mysql -DSEARCH_WXPDFDOCUMENT_PATH=${HOME}/.wxpdfdoc -DSEARCH_GEOS=1 -DSEARCH_GDAL=1 -DUSE_UNITTEST=1 -DCXXTEST_INCLUDE_DIR=/usr -DCXXTEST_PYTHON_TESTGEN_EXECUTABLE=/usr/bin/cxxtestgen -DUSE_APPIMAGE=1 ..
 make -j$(nproc)
