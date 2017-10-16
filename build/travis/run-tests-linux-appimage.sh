46502265a1ea790d50da2af1d48ce8e73c33a8ae
Pascal Horton <pascal.horton@terranum.ch>
2017-10-16 23:01:35 +0200

    Set AppImage build in a specific script

---
+++ build/travis/run-tests-linux-appimage.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+mkdir bin
+cd bin
+wget "https://github.com/AppImage/AppImageKit/releases/download/9/appimagetool-x86_64.AppImage"
+wget "https://github.com/AppImage/AppImageKit/releases/download/9/AppRun-x86_64"
+chmod a+x appimagetool-x86_64.AppImage
+chmod a+x AppRun-x86_64
+cmake -DSEARCH_GDAL_PATH=${HOME}/.gdal -DSEARCH_GEOS_PATH=/usr -DMYSQL_MAIN_DIR=${HOME}/.mysql -DSEARCH_WXPDFDOCUMENT_PATH=${HOME}/.wxpdfdoc -DSEARCH_GEOS=1 -DSEARCH_GDAL=1 -DUSE_UNITTEST=1 -DCXXTEST_INCLUDE_DIR=/usr -DCXXTEST_PYTHON_TESTGEN_EXECUTABLE=/usr/bin/cxxtestgen -DUSE_APPIMAGE=1 ..
+make -j$(nproc)
+echo $(find package/linux-appimage/ -name "*linux64.AppImage")
+cp $(find package/linux-appimage/ -name "*linux64.AppImage") .
