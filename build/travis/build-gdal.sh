a86d28e6f7db176780a9d7ed12eb3ecbd7ac8753
Pascal Horton <pascal.horton@terranum.ch>
2017-10-16 23:00:36 +0200

    Statically linking GDAL on Linux
    It makes the distribution easier as we rely on GDAL 2 that is not the system version.

--- build/travis/build-gdal.sh
+++ build/travis/build-gdal.sh
@@ -7,10 +7,10 @@ if [ ! "$(ls -A ${HOME}/.gdal)" ] || [ "$REBUILD_GDAL" = true ]; then
   if [ "$REBUILD_GDAL" = true ]; then
     rm -rf ${HOME}/.gdal
   fi
-  wget -q -O gdal.tar.gz "http://download.osgeo.org/gdal/2.1.3/gdal-2.1.3.tar.gz" > /dev/null
+  wget -q -O gdal.tar.gz "http://download.osgeo.org/gdal/2.2.2/gdal-2.2.2.tar.gz" > /dev/null
   tar -xzf gdal.tar.gz
-  cd gdal-2.1.3
-  ./configure --prefix=${HOME}/.gdal --with-geos=/usr/bin/geos-config --with-static-proj4=/usr --with-sqlite3=yes --with-python=no --with-pg=no --with-grass=no --with-jasper=no --with-jpeg=internal --with-png=internal --silent
+  cd gdal-2.2.2
+  ./configure --prefix=${HOME}/.gdal --with-geos=/usr/bin/geos-config --with-static-proj4=/usr --with-sqlite3=yes --with-python=no --with-pg=no --with-grass=no --with-jasper=no --with-jpeg=internal --with-png=internal --disable-shared --enable-static --silent
   make -j4 > /dev/null
   make install > /dev/null
   cd ..
