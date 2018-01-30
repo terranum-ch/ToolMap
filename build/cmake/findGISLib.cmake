f72fc3ba72a98abaf1d16b341586a18b6b05d8f6
Pascal Horton <pascal.horton@giub.unibe.ch>
2018-01-30 16:10:18 +0100

    Formatting CMake files.

--- build/cmake/findGISLib.cmake
+++ build/cmake/findGISLib.cmake
@@ -12,132 +12,124 @@
 
 #define what we search
 
-SET (SEARCH_GDAL CACHE BOOL "Sould we search for GDAL ?" )
-SET (SEARCH_GEOS CACHE BOOL "Sould we search for GEOS ?" )
-SET (SEARCH_GIS_LIB_PATH CACHE PATH "Path to a common folder containing GDAL, GEOS, PROJ and SQLITE")
-SET (SEARCH_GDAL_PATH CACHE PATH "Path to GDAL base")
-SET (SEARCH_GEOS_PATH CACHE PATH "Path to GEOS base")
-SET (SEARCH_PROJ_PATH CACHE PATH "Path to PROJ base")
-SET (SEARCH_SQLITE_PATH CACHE PATH "Path to SQLITE base")
+SET(SEARCH_GDAL CACHE BOOL "Sould we search for GDAL ?")
+SET(SEARCH_GEOS CACHE BOOL "Sould we search for GEOS ?")
+SET(SEARCH_GIS_LIB_PATH CACHE PATH "Path to a common folder containing GDAL, GEOS, PROJ and SQLITE")
+SET(SEARCH_GDAL_PATH CACHE PATH "Path to GDAL base")
+SET(SEARCH_GEOS_PATH CACHE PATH "Path to GEOS base")
+SET(SEARCH_PROJ_PATH CACHE PATH "Path to PROJ base")
+SET(SEARCH_SQLITE_PATH CACHE PATH "Path to SQLITE base")
 
 # if specific path aren't defined, use generic GIS Path
-if(NOT SEARCH_GDAL_PATH AND SEARCH_GIS_LIB_PATH)
-	SET(SEARCH_GDAL_PATH ${SEARCH_GIS_LIB_PATH})
-endif(NOT SEARCH_GDAL_PATH AND SEARCH_GIS_LIB_PATH)
+if (NOT SEARCH_GDAL_PATH AND SEARCH_GIS_LIB_PATH)
+    SET(SEARCH_GDAL_PATH ${SEARCH_GIS_LIB_PATH})
+endif (NOT SEARCH_GDAL_PATH AND SEARCH_GIS_LIB_PATH)
 
-if(NOT SEARCH_GEOS_PATH AND SEARCH_GIS_LIB_PATH)
-	SET(SEARCH_GEOS_PATH ${SEARCH_GIS_LIB_PATH})
-endif(NOT SEARCH_GEOS_PATH AND SEARCH_GIS_LIB_PATH)
+if (NOT SEARCH_GEOS_PATH AND SEARCH_GIS_LIB_PATH)
+    SET(SEARCH_GEOS_PATH ${SEARCH_GIS_LIB_PATH})
+endif (NOT SEARCH_GEOS_PATH AND SEARCH_GIS_LIB_PATH)
 
-if(NOT SEARCH_PROJ_PATH AND SEARCH_GIS_LIB_PATH)
-	SET(SEARCH_PROJ_PATH ${SEARCH_GIS_LIB_PATH})
-endif(NOT SEARCH_PROJ_PATH AND SEARCH_GIS_LIB_PATH)
+if (NOT SEARCH_PROJ_PATH AND SEARCH_GIS_LIB_PATH)
+    SET(SEARCH_PROJ_PATH ${SEARCH_GIS_LIB_PATH})
+endif (NOT SEARCH_PROJ_PATH AND SEARCH_GIS_LIB_PATH)
 
 # we search for geos lib
 IF (SEARCH_GEOS)
-	
-	# if windows and search geos
-	# using C API of GEOS:
-	IF (WIN32)
-		FIND_PATH(GEOS_C_INCLUDE_DIR geos_c.h
-  				HINTS			
-				${SEARCH_GEOS_PATH}/capi
-  				NO_DEFAULT_PATH)
-				
-		
-		FIND_PATH(GEOS_INCLUDE_DIR geos.h
-  			HINTS			
-			${SEARCH_GEOS_PATH}/source/headers
-			${SEARCH_GEOS_PATH}/include
-  			NO_DEFAULT_PATH)
-		
-  		FIND_LIBRARY(GEOS_LIBRARIES
-	  		geos_c_i NAMES geos_c
-	  		HINTS ${SEARCH_GEOS_PATH}/source
-			${SEARCH_GEOS_PATH}/src
-			${SEARCH_GEOS_PATH}
-			${SEARCH_GEOS_PATH}/lib/Release
-			NO_DEFAULT_PATH)
-	
-	# if unix / mac and search geos
-	ELSE (WIN32)
-		
-		IF(SEARCH_GEOS_PATH)
-			FIND_PATH(GEOS_INCLUDE_DIR geos_c.h
-	  				HINTS ${SEARCH_GEOS_PATH}/include
-				${SEARCH_GEOS_PATH}
-	  				NO_DEFAULT_PATH)
 
-	  		FIND_LIBRARY(GEOS_LIBRARIES
-		  		geos_c
-		  		HINTS ${SEARCH_GEOS_PATH}/lib	${SEARCH_GEOS_PATH} NO_DEFAULT_PATH)
-		ELSE(SEARCH_GEOS_PATH)
-			MESSAGE(STATUS "Searching GEOS into standard paths")
-			FIND_PATH(GEOS_INCLUDE_DIR geos_c.h
-	  				HINTS ${SEARCH_GEOS_PATH}/include
-				${SEARCH_GEOS_PATH})
+    # if windows and search geos
+    # using C API of GEOS:
+    IF (WIN32)
+        FIND_PATH(GEOS_C_INCLUDE_DIR geos_c.h
+                HINTS
+                ${SEARCH_GEOS_PATH}/capi
+                NO_DEFAULT_PATH)
 
-	  		FIND_LIBRARY(GEOS_LIBRARIES
-		  		geos_c
-		  		HINTS ${SEARCH_GEOS_PATH}/lib	${SEARCH_GEOS_PATH})
-		ENDIF(SEARCH_GEOS_PATH)
-		
-  		  		  			
-	ENDIF (WIN32)
 
-	MARK_AS_ADVANCED(
-				GEOS_INCLUDE_DIR
-				GEOS_LIBRARIES
-					)
+        FIND_PATH(GEOS_INCLUDE_DIR geos.h
+                HINTS
+                ${SEARCH_GEOS_PATH}/source/headers
+                ${SEARCH_GEOS_PATH}/include
+                NO_DEFAULT_PATH)
 
-  ## IF GEOS PARAMETERS ARE DEFINED, USE THEM
-   IF(GEOS_INCLUDE_DIR)
-    INCLUDE_DIRECTORIES(${GEOS_INCLUDE_DIR})
-    MESSAGE (STATUS "GEOS include is ${GEOS_INCLUDE_DIR}")
-  ENDIF(GEOS_INCLUDE_DIR)
- 
- 	IF (GEOS_C_INCLUDE_DIR)
-		MESSAGE(STATUS "GEOS C INCLUDE dir: ${GEOS_C_INCLUDE_DIR}")
-		INCLUDE_DIRECTORIES (${GEOS_C_INCLUDE_DIR})
-	ENDIF(GEOS_C_INCLUDE_DIR)
- 
- 
-  IF(GEOS_LIBRARIES)
-    LINK_LIBRARIES(${GEOS_LIBRARIES})
-    MESSAGE (STATUS "GEOS lib is ${GEOS_LIBRARIES}")
-  ENDIF(GEOS_LIBRARIES)
+        FIND_LIBRARY(GEOS_LIBRARIES
+                geos_c_i NAMES geos_c
+                HINTS ${SEARCH_GEOS_PATH}/source
+                ${SEARCH_GEOS_PATH}/src
+                ${SEARCH_GEOS_PATH}
+                ${SEARCH_GEOS_PATH}/lib/Release
+                NO_DEFAULT_PATH)
+
+        # if unix / mac and search geos
+    ELSE (WIN32)
+
+        IF (SEARCH_GEOS_PATH)
+            FIND_PATH(GEOS_INCLUDE_DIR geos_c.h
+                    HINTS ${SEARCH_GEOS_PATH}/include
+                    ${SEARCH_GEOS_PATH}
+                    NO_DEFAULT_PATH)
+
+            FIND_LIBRARY(GEOS_LIBRARIES
+                    geos_c
+                    HINTS ${SEARCH_GEOS_PATH}/lib ${SEARCH_GEOS_PATH} NO_DEFAULT_PATH)
+        ELSE (SEARCH_GEOS_PATH)
+            MESSAGE(STATUS "Searching GEOS into standard paths")
+            FIND_PATH(GEOS_INCLUDE_DIR geos_c.h
+                    HINTS ${SEARCH_GEOS_PATH}/include
+                    ${SEARCH_GEOS_PATH})
+
+            FIND_LIBRARY(GEOS_LIBRARIES
+                    geos_c
+                    HINTS ${SEARCH_GEOS_PATH}/lib ${SEARCH_GEOS_PATH})
+        ENDIF (SEARCH_GEOS_PATH)
+
+
+    ENDIF (WIN32)
+
+    MARK_AS_ADVANCED(
+            GEOS_INCLUDE_DIR
+            GEOS_LIBRARIES
+    )
+
+    ## IF GEOS PARAMETERS ARE DEFINED, USE THEM
+    IF (GEOS_INCLUDE_DIR)
+        INCLUDE_DIRECTORIES(${GEOS_INCLUDE_DIR})
+        MESSAGE(STATUS "GEOS include is ${GEOS_INCLUDE_DIR}")
+    ENDIF (GEOS_INCLUDE_DIR)
+
+    IF (GEOS_C_INCLUDE_DIR)
+        MESSAGE(STATUS "GEOS C INCLUDE dir: ${GEOS_C_INCLUDE_DIR}")
+        INCLUDE_DIRECTORIES(${GEOS_C_INCLUDE_DIR})
+    ENDIF (GEOS_C_INCLUDE_DIR)
+
+
+    IF (GEOS_LIBRARIES)
+        LINK_LIBRARIES(${GEOS_LIBRARIES})
+        MESSAGE(STATUS "GEOS lib is ${GEOS_LIBRARIES}")
+    ENDIF (GEOS_LIBRARIES)
 
 ENDIF (SEARCH_GEOS)
 #end search for geos
 
 
-
-
-
-
-
-
-
-
 #search for GDAL lib
 IF (SEARCH_GDAL)
     # if windows
-	IF (WIN32)
-		
-		FIND_PATH(GDAL_INCLUDE_DIR gdal.h
-  				HINTS ${SEARCH_GDAL_PATH}/include
-			${SEARCH_GDAL_PATH}
+    IF (WIN32)
+
+        FIND_PATH(GDAL_INCLUDE_DIR gdal.h
+                HINTS ${SEARCH_GDAL_PATH}/include
+                ${SEARCH_GDAL_PATH}
                 NO_DEFAULT_PATH)
-  		
-  		FIND_LIBRARY(GDAL_LIBRARIES
-	  		gdal_i
-	  		HINTS ${SEARCH_GDAL_PATH}/lib
-		${SEARCH_GDAL_PATH}
-  			NO_DEFAULT_PATH)
-	
-	# if mac, use the dynamic library
-	ELSEIF (APPLE)
-        IF(SEARCH_GDAL_PATH)
+
+        FIND_LIBRARY(GDAL_LIBRARIES
+                gdal_i
+                HINTS ${SEARCH_GDAL_PATH}/lib
+                ${SEARCH_GDAL_PATH}
+                NO_DEFAULT_PATH)
+
+        # if mac, use the dynamic library
+    ELSEIF (APPLE)
+        IF (SEARCH_GDAL_PATH)
             FIND_PATH(GDAL_INCLUDE_DIR gdal.h
                     PATHS ${SEARCH_GDAL_PATH}/include
                     ${SEARCH_GDAL_PATH}
@@ -149,7 +141,7 @@ IF (SEARCH_GDAL)
                     PATHS ${SEARCH_GDAL_PATH}/lib
                     ${SEARCH_GDAL_PATH} NO_DEFAULT_PATH)
 
-        ELSE(SEARCH_GDAL_PATH)
+        ELSE (SEARCH_GDAL_PATH)
             MESSAGE(STATUS "Searching GDAL on standard PATHS")
             FIND_PATH(GDAL_INCLUDE_DIR gdal.h
                     HINTS ${SEARCH_GDAL_PATH}/include
@@ -158,66 +150,66 @@ IF (SEARCH_GDAL)
 
             FIND_LIBRARY(GDAL_LIBRARIES
                     gdal NAMES gdal1 gdal1.6.0 gdal1.7.0 gdal1.8.0 gdal1.9.0)
-        ENDIF(SEARCH_GDAL_PATH)
+        ENDIF (SEARCH_GDAL_PATH)
 
-    # if linux, use the static library
-    ELSE()
-		IF(SEARCH_GDAL_PATH)
-			FIND_PATH(GDAL_INCLUDE_DIR gdal.h
-	  				PATHS ${SEARCH_GDAL_PATH}/include
-	  				${SEARCH_GDAL_PATH}
-	                PATH_SUFFIXES gdal  				
-	  				NO_DEFAULT_PATH)
+        # if linux, use the static library
+    ELSE ()
+        IF (SEARCH_GDAL_PATH)
+            FIND_PATH(GDAL_INCLUDE_DIR gdal.h
+                    PATHS ${SEARCH_GDAL_PATH}/include
+                    ${SEARCH_GDAL_PATH}
+                    PATH_SUFFIXES gdal
+                    NO_DEFAULT_PATH)
 
-	  		FIND_LIBRARY(GDAL_LIBRARIES
-		  		NAMES libgdal.a gdal1 gdal1.6.0 gdal1.7.0 gdal1.8.0 gdal1.9.0
-		  		PATHS ${SEARCH_GDAL_PATH}/lib
-	  			${SEARCH_GDAL_PATH} NO_DEFAULT_PATH)
+            FIND_LIBRARY(GDAL_LIBRARIES
+                    NAMES libgdal.a gdal1 gdal1.6.0 gdal1.7.0 gdal1.8.0 gdal1.9.0
+                    PATHS ${SEARCH_GDAL_PATH}/lib
+                    ${SEARCH_GDAL_PATH} NO_DEFAULT_PATH)
 
             FIND_PROGRAM(GDAL_CONFIG gdal-config
-                ${SEARCH_GDAL_PATH}/bin/
-                NO_DEFAULT_PATH)
+                    ${SEARCH_GDAL_PATH}/bin/
+                    NO_DEFAULT_PATH)
 
-		ELSE(SEARCH_GDAL_PATH)
-			MESSAGE(STATUS "Searching GDAL on standard PATHS")
-			FIND_PATH(GDAL_INCLUDE_DIR gdal.h
-	  				HINTS ${SEARCH_GDAL_PATH}/include
-	  				${SEARCH_GDAL_PATH}
-	                PATH_SUFFIXES gdal)
+        ELSE (SEARCH_GDAL_PATH)
+            MESSAGE(STATUS "Searching GDAL on standard PATHS")
+            FIND_PATH(GDAL_INCLUDE_DIR gdal.h
+                    HINTS ${SEARCH_GDAL_PATH}/include
+                    ${SEARCH_GDAL_PATH}
+                    PATH_SUFFIXES gdal)
 
-	  		FIND_LIBRARY(GDAL_LIBRARIES
-		  		NAMES libgdal.a gdal1 gdal1.6.0 gdal1.7.0 gdal1.8.0 gdal1.9.0)
+            FIND_LIBRARY(GDAL_LIBRARIES
+                    NAMES libgdal.a gdal1 gdal1.6.0 gdal1.7.0 gdal1.8.0 gdal1.9.0)
 
             FIND_PROGRAM(GDAL_CONFIG gdal-config)
 
-		ENDIF(SEARCH_GDAL_PATH)
+        ENDIF (SEARCH_GDAL_PATH)
 
-        IF(GDAL_CONFIG)
+        IF (GDAL_CONFIG)
             EXEC_PROGRAM(${GDAL_CONFIG} ARGS --dep-libs OUTPUT_VARIABLE GDAL_DEP_LIBS)
             list(APPEND GDAL_LIBRARIES ${GDAL_DEP_LIBS})
-        ENDIF(GDAL_CONFIG)
-  		  		  			
-	ENDIF (WIN32)
-	
-	
-	MARK_AS_ADVANCED(
-				GDAL_INCLUDE_DIR
-				GDAL_LIBRARIES
-					)
+        ENDIF (GDAL_CONFIG)
 
-  
-  ## IF GDAL PARAMETERS ARE DEFINED, USE THEM
-   IF(GDAL_INCLUDE_DIR)
-    INCLUDE_DIRECTORIES(${GDAL_INCLUDE_DIR})
-  ENDIF(GDAL_INCLUDE_DIR)
+    ENDIF (WIN32)
 
-  IF(GDAL_LIBRARIES)
-    LINK_LIBRARIES(${GDAL_LIBRARIES})
-  ENDIF(GDAL_LIBRARIES)
-	
-	#debug message
-	MESSAGE (STATUS "GDAL lib is ${GDAL_LIBRARIES}")
-	MESSAGE (STATUS "GDAL include is ${GDAL_INCLUDE_DIR}")
+
+    MARK_AS_ADVANCED(
+            GDAL_INCLUDE_DIR
+            GDAL_LIBRARIES
+    )
+
+
+    ## IF GDAL PARAMETERS ARE DEFINED, USE THEM
+    IF (GDAL_INCLUDE_DIR)
+        INCLUDE_DIRECTORIES(${GDAL_INCLUDE_DIR})
+    ENDIF (GDAL_INCLUDE_DIR)
+
+    IF (GDAL_LIBRARIES)
+        LINK_LIBRARIES(${GDAL_LIBRARIES})
+    ENDIF (GDAL_LIBRARIES)
+
+    #debug message
+    MESSAGE(STATUS "GDAL lib is ${GDAL_LIBRARIES}")
+    MESSAGE(STATUS "GDAL include is ${GDAL_INCLUDE_DIR}")
 
 ENDIF (SEARCH_GDAL)
 #end search for GDAL lib
--- build/cmake/useMySQLEmbedded.cmake
+++ build/cmake/useMySQLEmbedded.cmake
@@ -2,71 +2,71 @@
 # THIS WILL ONLY WORK IF MYSQL WAS FOUND
 # (c) Lucien Schreiber CREALP 2012
 
-SET (MYSQL_ERRSQL_FROM_PATH "")
-SET (MYSQL_ERRSQL_TO_PATH "")
+SET(MYSQL_ERRSQL_FROM_PATH "")
+SET(MYSQL_ERRSQL_TO_PATH "")
 
 IF (MYSQL_INCLUDE_DIR)
-	MESSAGE (STATUS "MYSQL Include dir defined : ${MYSQL_INCLUDE_DIR}")
-	SET (MYSQL_SHARE_DIR "${MYSQL_INCLUDE_DIR}../share/english")
-	FIND_FILE(MYSQL_ERRMSG_FILE
-			errmsg.sys
-			PATHS
-			${MYSQL_INCLUDE_DIR}../share/mysql/english
-			${MYSQL_INCLUDE_DIR}/../share/mysql/english
-			${MYSQL_INCLUDE_DIR}../share/english
-			${MYSQL_INCLUDE_DIR}/../share/english
-			${MYSQL_INCLUDE_DIR}../share/
-			${MYSQL_INCLUDE_DIR}/../share/
-			)
-	IF(NOT MYSQL_ERRMSG_FILE)
-		MESSAGE(WARNING "errmsg.sys file not found!")
-	ENDIF()
+    MESSAGE(STATUS "MYSQL Include dir defined : ${MYSQL_INCLUDE_DIR}")
+    SET(MYSQL_SHARE_DIR "${MYSQL_INCLUDE_DIR}../share/english")
+    FIND_FILE(MYSQL_ERRMSG_FILE
+            errmsg.sys
+            PATHS
+            ${MYSQL_INCLUDE_DIR}../share/mysql/english
+            ${MYSQL_INCLUDE_DIR}/../share/mysql/english
+            ${MYSQL_INCLUDE_DIR}../share/english
+            ${MYSQL_INCLUDE_DIR}/../share/english
+            ${MYSQL_INCLUDE_DIR}../share/
+            ${MYSQL_INCLUDE_DIR}/../share/
+            )
+    IF (NOT MYSQL_ERRMSG_FILE)
+        MESSAGE(WARNING "errmsg.sys file not found!")
+    ENDIF ()
 
-	IF(APPLE)
-		ADD_CUSTOM_COMMAND(TARGET ${CMAKE_PROJECT_NAME}
-			POST_BUILD
-			COMMAND mkdir -p
-			 ${PROJECT_BINARY_DIR}/${CMAKE_CFG_INTDIR}/${CMAKE_PROJECT_NAME}.app/Contents/mysql
-			COMMAND cp ${MYSQL_ERRMSG_FILE}
-			${PROJECT_BINARY_DIR}/${CMAKE_CFG_INTDIR}/${CMAKE_PROJECT_NAME}.app/Contents/mysql/errmsg.sys
-			)
-	ENDIF(APPLE)
+    IF (APPLE)
+        ADD_CUSTOM_COMMAND(TARGET ${CMAKE_PROJECT_NAME}
+                POST_BUILD
+                COMMAND mkdir -p
+                ${PROJECT_BINARY_DIR}/${CMAKE_CFG_INTDIR}/${CMAKE_PROJECT_NAME}.app/Contents/mysql
+                COMMAND cp ${MYSQL_ERRMSG_FILE}
+                ${PROJECT_BINARY_DIR}/${CMAKE_CFG_INTDIR}/${CMAKE_PROJECT_NAME}.app/Contents/mysql/errmsg.sys
+                )
+    ENDIF (APPLE)
 
 
-    IF(WIN32)
-        FILE (TO_NATIVE_PATH ${PROJECT_BINARY_DIR}/${CMAKE_CFG_INTDIR}/mysql WIN_MYSQL_DIR)
-        FILE (TO_NATIVE_PATH ${MYSQL_SHARE_DIR}/errmsg.sys  WIN_MYSQL_ERRMSG_SOURCE)
-        FILE (TO_NATIVE_PATH ${PROJECT_BINARY_DIR}/${CMAKE_CFG_INTDIR}/mysql/errmsg.sys  WIN_MYSQL_ERRMSG_DEST)
+    IF (WIN32)
+        FILE(TO_NATIVE_PATH ${PROJECT_BINARY_DIR}/${CMAKE_CFG_INTDIR}/mysql WIN_MYSQL_DIR)
+        FILE(TO_NATIVE_PATH ${MYSQL_SHARE_DIR}/errmsg.sys WIN_MYSQL_ERRMSG_SOURCE)
+        FILE(TO_NATIVE_PATH ${PROJECT_BINARY_DIR}/${CMAKE_CFG_INTDIR}/mysql/errmsg.sys WIN_MYSQL_ERRMSG_DEST)
 
         ADD_CUSTOM_COMMAND(TARGET ${CMAKE_PROJECT_NAME}
                 POST_BUILD
                 COMMAND IF exist ${WIN_MYSQL_DIR} (echo ${WIN_MYSQL_DIR} exists) ELSE (mkdir ${WIN_MYSQL_DIR})
-                COMMAND ${CMAKE_COMMAND} -E copy_if_different ${MYSQL_ERRMSG_FILE}	$<TARGET_FILE_DIR:${CMAKE_PROJECT_NAME}>/mysql/
+                COMMAND ${CMAKE_COMMAND} -E copy_if_different ${MYSQL_ERRMSG_FILE} $<TARGET_FILE_DIR:${CMAKE_PROJECT_NAME}>/mysql/
                 )
 
         # Copy optimized MySQL  dll under WINDOWS
-        FIND_FILE (MYSQL_DLL_NAME
+        FIND_FILE(MYSQL_DLL_NAME
                 libmysqld.dll
                 HINTS ${MYSQL_MAIN_DIR}/Embedded/DLL/release
                 ${MYSQL_MAIN_DIR}/lib
                 NO_DEFAULT_PATH)
         IF (NOT MYSQL_DLL_NAME)
-            MESSAGE (SEND_ERROR "MySQL dll not found in ${MYSQL_MAIN_DIR}")
-        ENDIF()
+            MESSAGE(SEND_ERROR "MySQL dll not found in ${MYSQL_MAIN_DIR}")
+        ENDIF ()
 
-        ADD_CUSTOM_COMMAND (TARGET ${CMAKE_PROJECT_NAME} POST_BUILD
+        ADD_CUSTOM_COMMAND(TARGET ${CMAKE_PROJECT_NAME} POST_BUILD
                 COMMAND ${CMAKE_COMMAND} -E copy_if_different
                 "${MYSQL_DLL_NAME}"
                 "${PROJECT_BINARY_DIR}/${CMAKE_CFG_INTDIR}")
-    ENDIF(WIN32)
+    ENDIF (WIN32)
 
     # COPY errmsg.sys under Linux
-    IF(UNIX AND NOT APPLE)
+    IF (UNIX AND NOT APPLE)
         FILE(COPY ${MYSQL_ERRMSG_FILE}
                 DESTINATION ${PROJECT_BINARY_DIR}/mysql)
-    ENDIF(UNIX AND NOT APPLE)
+    ENDIF (UNIX AND NOT APPLE)
 
-ENDIF(MYSQL_INCLUDE_DIR)
+ENDIF (MYSQL_INCLUDE_DIR)
 
 
 
