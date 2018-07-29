7a0d16e27dc33e90e6d0868f0077e973730fbff6
Pascal Horton <pascal.horton@terranum.ch>
2018-07-29 11:10:11 +0200

    Fixing paths for Travis.

--- ci/travis/dependencies-osx.sh
+++ ci/travis/dependencies-osx.sh
@@ -7,14 +7,14 @@ wget -q -O cxxtest.tar.gz "https://github.com/CxxTest/cxxtest/archive/4.3.tar.gz
 tar -xzf cxxtest.tar.gz -C ${HOME}
 
 # Build libraries
-chmod +x build/travis/build-wxwidgets-osx.sh
-build/travis/build-wxwidgets-osx.sh
-chmod +x build/travis/build-gdal-osx.sh
-build/travis/build-gdal-osx.sh
-chmod +x build/travis/build-wxpdfdoc.sh
-build/travis/build-wxpdfdoc.sh
-chmod +x build/travis/build-mysql.sh
-build/travis/build-mysql.sh
+chmod +x ci/travis/build-wxwidgets-osx.sh
+ci/travis/build-wxwidgets-osx.sh
+chmod +x ci/travis/build-gdal-osx.sh
+ci/travis/build-gdal-osx.sh
+chmod +x ci/travis/build-wxpdfdoc.sh
+ci/travis/build-wxpdfdoc.sh
+chmod +x ci/travis/build-mysql.sh
+ci/travis/build-mysql.sh
 
 # Changing permissions of Homebrew libraries
 sudo chmod -R 777 /usr/local/Cellar
