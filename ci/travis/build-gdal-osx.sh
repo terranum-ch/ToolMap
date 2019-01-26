983f29d6e345d22c80193321a13ad723eb4935e5
Pascal Horton <pascal.horton@terranum.ch>
2019-01-26 22:48:22 +0100

    Updating GDAL version.

--- ci/travis/build-gdal-osx.sh
+++ ci/travis/build-gdal-osx.sh
@@ -7,9 +7,9 @@ if [ ! "$(ls -A ${HOME}/.gdal)" ] || [ "$REBUILD_GDAL" = true ]; then
   if [ "$REBUILD_GDAL" = true ]; then
     rm -rf ${HOME}/.gdal
   fi
-  wget -q -O gdal.tar.gz "http://download.osgeo.org/gdal/2.1.3/gdal-2.1.3.tar.gz" > /dev/null
+  wget -q -O gdal.tar.gz "http://download.osgeo.org/gdal/2.4.0/gdal-2.4.0.tar.gz" > /dev/null
   tar -xzf gdal.tar.gz
-  cd gdal-2.1.3
+  cd gdal-2.4.0
   ./configure --prefix=${HOME}/.gdal --with-geos=/usr/local/bin/geos-config --with-static-proj4=/usr --with-sqlite3=yes --with-python=no --with-pg=no --with-grass=no --with-jasper=no --with-jpeg=internal --with-png=internal --silent
   make -j4 > /dev/null
   make install > /dev/null
