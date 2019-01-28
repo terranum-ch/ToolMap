ee61eb285815309aba8b5d3a952e78399324afa3
Pascal Horton <pascal.horton@terranum.ch>
2019-01-28 13:13:00 +0100

    Debugging Travis osx.

--- ci/travis/build-gdal-osx.sh
+++ ci/travis/build-gdal-osx.sh
@@ -10,6 +10,14 @@ if [ ! "$(ls -A ${HOME}/.gdal)" ] || [ "$REBUILD_GDAL" = true ]; then
   wget -q -O gdal.tar.gz "http://download.osgeo.org/gdal/2.4.0/gdal-2.4.0.tar.gz" > /dev/null
   tar -xzf gdal.tar.gz
   cd gdal-2.4.0
+
+  curl-config --version
+  /usr/bin/curl-config --version
+  /usr/bin/curl/curl-config --version
+  /usr/local/curl-config --version
+  /usr/local/curl/curl-config --version
+  ${HOMEBREW_PREFIX}/opt/curl/bin/curl-config --version
+
   ./configure --prefix=${HOME}/.gdal --with-geos=/usr/bin/geos-config --with-curl=/usr --with-proj=/usr --with-sqlite3=yes --with-python=no --with-pg=no --with-grass=no --with-jasper=no --with-jpeg=internal --with-png=internal --with-threads=no --silent
   make -j4 > /dev/null
   make install > /dev/null
