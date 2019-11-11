4db8b83ca69bf4065e81db6e33fd4c000f688c36
Pascal Horton <pascal.horton@terranum.ch>
2019-11-11 19:30:44 +0100

    Looking for proj as a static library on Travis CI.

--- ci/travis/build-proj.sh
+++ ci/travis/build-proj.sh
@@ -10,7 +10,7 @@ if [ ! "$(ls -A ${HOME}/.proj)" ] || [ "$REBUILD_PROJ" = true ]; then
   wget -q -O proj.tar.gz "https://github.com/OSGeo/PROJ/releases/download/6.2.1/proj-6.2.1.tar.gz" > /dev/null
   tar -xzf proj.tar.gz
   cd proj-6.2.1
-  ./configure --prefix=${HOME}/.proj --silent
+  ./configure --prefix=${HOME}/.proj --disable-shared --silent
   make -j4 > /dev/null
   make install > /dev/null
   cd ..
