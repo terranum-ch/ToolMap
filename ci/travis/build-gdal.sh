21aa595f02f09355c8e8a73d3d551c8de96eb4ab
Pascal Horton <pascal.horton@terranum.ch>
2019-11-12 08:35:04 +0100

    Working on Travis CI.

--- ci/travis/build-gdal.sh
+++ ci/travis/build-gdal.sh
@@ -10,7 +10,7 @@ if [ ! "$(ls -A ${HOME}/.gdal)" ] || [ "$REBUILD_GDAL" = true ]; then
   wget -q -O gdal.tar.gz "https://github.com/OSGeo/gdal/releases/download/v3.0.2/gdal-3.0.2.tar.gz" > /dev/null
   tar -xzf gdal.tar.gz
   cd gdal-3.0.2
-  ./configure --prefix=${HOME}/.gdal --with-geos=/usr/bin/geos-config --with-proj=usr/local --with-sqlite3=yes --with-python=no --with-pg=no --with-grass=no --with-jasper=no --with-jpeg=internal --with-png=internal --disable-shared --enable-static --silent
+  ./configure --prefix=${HOME}/.gdal --with-geos=/usr/bin/geos-config --with-proj=/usr/local --with-sqlite3=yes --with-python=no --with-pg=no --with-grass=no --with-jasper=no --with-jpeg=internal --with-png=internal --disable-shared --enable-static --silent
   make -j4 > /dev/null
   make install > /dev/null
   cd ..
