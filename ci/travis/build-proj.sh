aaef8f1c87da5e9e0cac6bcc3eb60feee42626e4
Pascal Horton <pascal.horton@terranum.ch>
2019-11-12 09:49:59 +0100

    Working on Travis CI.

--- ci/travis/build-proj.sh
+++ ci/travis/build-proj.sh
@@ -4,7 +4,8 @@
 wget -q -O proj.tar.gz "https://github.com/OSGeo/PROJ/releases/download/6.2.1/proj-6.2.1.tar.gz" > /dev/null
 tar -xzf proj.tar.gz
 cd proj-6.2.1
-./configure --prefix=/usr/local --silent
-make -j4 > /dev/null
-sudo make install > /dev/null
+./configure --prefix=/usr/local --enable-shared #--silent
+make -j4 #> /dev/null
+sudo make install #> /dev/null
+ls /usr/local/lib
 cd ..
