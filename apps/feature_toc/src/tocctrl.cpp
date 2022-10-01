3427f45921c208c8c76d6311b0b562148dcf62c4
Lucien SCHREIBER <lucien.schreiber@terranum.ch>
2022-10-01 17:25:45 +0200

    see #477 adding code for DND

--- apps/feature_toc/src/tocctrl.cpp
+++ apps/feature_toc/src/tocctrl.cpp
@@ -3,37 +3,36 @@
 //
 
 #include "tocctrl.h"
+
 #include "bitmaps.h"
 
-TocCtrl::TocCtrl(wxWindow *parent, wxWindowID id) : wxDataViewCtrl(parent, id, wxDefaultPosition, wxDefaultSize,
-                                                                       wxDV_NO_HEADER | wxDV_SINGLE) {
-    // Setting model
-    wxObjectDataPtr<wxDataViewModel> my_model  (new TocCtrlModel);
-    wxDataViewCtrl::AssociateModel( my_model.get() );
+TocCtrl::TocCtrl(wxWindow *parent, wxWindowID id)
+    : wxDataViewCtrl(parent, id, wxDefaultPosition, wxDefaultSize, wxDV_NO_HEADER | wxDV_SINGLE) {
+  // Setting model
+  wxObjectDataPtr<wxDataViewModel> my_model(new TocCtrlModel);
+  wxDataViewCtrl::AssociateModel(my_model.get());
 
-    // Column definition
-    wxDataViewCheckIconTextRenderer * renderer = new wxDataViewCheckIconTextRenderer(  );
-    wxDataViewColumn *col1 = new wxDataViewColumn("test", renderer, 0, wxDVC_DEFAULT_WIDTH, wxALIGN_LEFT);
-    wxDataViewCtrl::AppendColumn(col1);
+  // Column definition
+  wxDataViewCheckIconTextRenderer *renderer = new wxDataViewCheckIconTextRenderer();
+  wxDataViewColumn *col1 = new wxDataViewColumn("test", renderer, 0, wxDVC_DEFAULT_WIDTH, wxALIGN_LEFT);
+  wxDataViewCtrl::AppendColumn(col1);
 
-    // events
-    Bind(wxEVT_DATAVIEW_ITEM_BEGIN_DRAG, &TocCtrl::on_dragndrop_begin, this);
+  // events
+  this->Bind(wxEVT_DATAVIEW_ITEM_BEGIN_DRAG, &TocCtrl::on_dragndrop_begin, this, id);
 }
 
 void TocCtrl::add_test_data() {
+  //    wxDataViewItem group = AppendContainer(wxDataViewCtrl::GetTopItem(), "Group");
+  //    wxDataViewItem item = AppendItem(group, "Salut");
+  //    SetItemIcon(item,
+  //                wxBitmapBundle::FromSVG(feature_toc_bitmaps::toc_image, wxSize(16, 16)).GetBitmap(wxSize(16, 16)));
 
-//    wxDataViewItem group = AppendContainer(wxDataViewCtrl::GetTopItem(), "Group");
-//    wxDataViewItem item = AppendItem(group, "Salut");
-//    SetItemIcon(item,
-//                wxBitmapBundle::FromSVG(feature_toc_bitmaps::toc_image, wxSize(16, 16)).GetBitmap(wxSize(16, 16)));
-
-
-//    wxTreeListItem group = add_group(GetRootItem(), "Group");
-//    wxTreeListItem item2 = add_layer(group, "Shapefile.shp", 1);
-//    wxTreeListItem group2 = add_group(group, "Project Layers");
-//    wxTreeListItem item3 = add_layer(group2, "Line", 2);
-//    wxTreeListItem item4 = add_layer(group2, "Points", 2);
-//    wxTreeListItem item5 = add_layer(group, "Raster", 3);
+  //    wxTreeListItem group = add_group(GetRootItem(), "Group");
+  //    wxTreeListItem item2 = add_layer(group, "Shapefile.shp", 1);
+  //    wxTreeListItem group2 = add_group(group, "Project Layers");
+  //    wxTreeListItem item3 = add_layer(group2, "Line", 2);
+  //    wxTreeListItem item4 = add_layer(group2, "Points", 2);
+  //    wxTreeListItem item5 = add_layer(group, "Raster", 3);
 }
 
 /*
@@ -55,15 +54,21 @@ wxTreeListItem TocCtrl::add_layer(wxTreeListItem parent, const wxString &label, 
 }*/
 
 void TocCtrl::on_dragndrop_begin(wxDataViewEvent &event) {
-    event.Allow();
-//    wxLogError("testing dragndrop");
-//    wxDataViewItem dragItem = event.GetItem();
-//    if (!dragItem) {
-//        event.Veto();
-//        return;
-//    }
-//
-//    wxTextDataObject *obj = new wxTextDataObject();
-//    event.SetDataObject(obj);
-//    event.SetDragFlags(wxDrag_AllowMove);
+  wxDataViewItem item( event.GetItem() );
+
+  // only allow drags for item, not containers
+  if (GetModel()->IsContainer( item ) )
+  {
+    wxLogMessage("Forbidding starting dragging");
+    event.Veto();
+    return;
+  }
+
+  TocCtrlModelNode *node = (TocCtrlModelNode *) item.GetID();
+  wxTextDataObject *obj = new wxTextDataObject;
+  obj->SetText( node->m_title );
+  event.SetDataObject( obj );
+  event.SetDragFlags(wxDrag_AllowMove); // allows both copy and move
+
+  wxLogMessage("Starting dragging \"%s\"", node->m_title);
 }
