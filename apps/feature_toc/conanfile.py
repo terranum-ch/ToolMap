4996fdf91013169110bca72b82b44ba57f4b4d8d
Lucien SCHREIBER <lucien.schreiber@terranum.ch>
2022-10-20 11:08:53 +0200

    adding option for choosing wxWidgets for Linux

--- apps/feature_toc/conanfile.py
+++ apps/feature_toc/conanfile.py
@@ -3,14 +3,18 @@ from conans import ConanFile, CMake
 
 class FeatureTOC(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    # requires = ["wxwidgets/3.1.7@terranum-conan+wxwidgets/stable"]
-
     generators = "cmake", "gcc", "txt"
+
+    options = {"old_linux_wxwidgets": [True, False]}
+    default_options = {"old_linux_wxwidgets": False}
 
     def configure(self):
         if self.settings.os == "Linux":
-            self.requires('wxwidgets/3.1.7@terranum-conan+wxwidgets/stable')
-            self.options["wxwidgets"].webview = False  # webview control isn't available on linux.
+            if self.options.old_linux_wxwidgets:
+                self.requires('wxwidgets/3.1.7@terranum-conan+wxwidgets/stable')
+                self.options["wxwidgets"].webview = False  # webview control isn't available on linux.
+            else:
+                self.requires('wxwidgets/3.2.1@terranum-conan+wxwidgets/stable')
         else:
             self.requires('wxwidgets/3.2.1@terranum-conan+wxwidgets/stable')
 
