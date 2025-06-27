268229862bc060a5d1e4d7cb78f6e2d1dddfc5c4
Lucien <lucien.schreiber@terranum.ch>
2025-06-27 11:26:29 +0200

    see #514 adding unit test for reading WMS file

--- test/src/test_tmwms.cpp
+++ test/src/test_tmwms.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 #include "test_param.h"
 #include "../../src/gis/tmwms.h"
+#include "../../src/gis/tmgisdataraster.h"
 #include "../../src/gis/tmgisdata.h"
 #include <wx/filename.h>
 
@@ -66,5 +67,68 @@ TEST_F(TestWMSXml, CreateXML) {
         wxRemoveFile(xml_output.GetFullPath());
     }
     EXPECT_TRUE(wmsFileXML.CreateXML("ch.swisstopo.geologie-geocover", xml_output.GetFullPath()));
+}
+
+TEST_F(TestWMSXml, OpenLayer) {
+    tmWMSFileXML wmsFileXML("https://wms.geo.admin.ch/?SERVICE=WMS&VERSION=1.3.0");
+    EXPECT_EQ(wmsFileXML.GetWMSUrl(), "https://wms.geo.admin.ch/?SERVICE=WMS&VERSION=1.3.0");
+    wxFileName xml_output(g_TestPathEXPORT + _T("/wms_layer_output.xml"));
+    wxLogMessage("Xml Output: '%s'", xml_output.GetFullPath());
+    if (xml_output.Exists()) {
+        wxRemoveFile(xml_output.GetFullPath());
+    }
+    EXPECT_TRUE(wmsFileXML.CreateXML("ch.swisstopo.geologie-geocover", xml_output.GetFullPath()));
+
+    tmLayerProperties* item = new tmLayerProperties();
+    item->InitFromPathAndName(xml_output.GetPath(), xml_output.GetFullName(),
+                              tmGISData::GetAllSupportedGISFormatsExtensions());
+
+    // try to open the file for getting the spatial type
+    tmGISData* myLayer = tmGISData::LoadLayer(item);
+    if (myLayer == nullptr) {
+        wxLogError(_("Not able to open the layer : %s"), item->GetNameDisplay().c_str());
+        wxDELETE(item);
+    }
+    EXPECT_NE(myLayer, nullptr);
+
+    // try to open the layer with gdal
+    tmGISDataRaster* rasterLayer = dynamic_cast<tmGISDataRaster*>(myLayer);
+    EXPECT_NE(rasterLayer, nullptr);
+    int bandCount = rasterLayer->GetBandCount();
+    EXPECT_EQ(bandCount, 3);
+
+    wxString metadata_html = rasterLayer->GetMetaDataAsHtml();
+    wxLogMessage("Metadata HTML: %s", metadata_html);
+    EXPECT_FALSE(metadata_html.IsEmpty());
+
+    // trying to get image data
+    // owned by image, do not destroy manually.
+    unsigned char* imgbuf;
+    unsigned int imglen;
+    unsigned char* maskbuf;
+    unsigned int masklen;
+
+    tmCoordConvert coordConvert(PROJ_SWISS_CH1903);
+    rasterLayer->SetCoordConvert(&coordConvert);
+
+    tmRealRect my_reel_coord(496732.99, 116284.07, 506000.56, 123906.90);
+    EXPECT_TRUE(rasterLayer->SetSpatialFilter(my_reel_coord, item->GetType()));
+
+    if (rasterLayer->GetImageData(&imgbuf, &imglen, &maskbuf, &masklen, wxSize(100, 100)) != CE_None) {
+        fprintf(stderr, "%s line %d : Error loading image data \n ", __FUNCTION__, __LINE__);
+        // need cleanup
+        // in all case, clean data
+        if (imgbuf) {
+            CPLFree(imgbuf);
+            imgbuf = nullptr;
+        }
+
+        if (maskbuf) {
+            CPLFree(maskbuf);
+            maskbuf = nullptr;
+        }
+        FAIL();
+    }
+    SUCCEED();
 }
 
