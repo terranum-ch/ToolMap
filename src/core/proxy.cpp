20b67299900eb9fb26656d496939fe5984397a36
Pascal Horton <pascal.horton@terranum.ch>
2025-07-18 16:29:47 +0200

    Implementing a proxy automatic configuration (PAC) support.

--- src/core/proxy.cpp
+++ src/core/proxy.cpp
@@ -13,24 +13,56 @@
 #include <wx/fileconf.h>
 
 wxString GetSystemProxy() {
+    wxString proxyInfo = wxEmptyString;
+
 #ifdef __WXMSW__
-    WINHTTP_CURRENT_USER_IE_PROXY_CONFIG proxyConfig;
-    if (WinHttpGetIEProxyConfigForCurrentUser(&proxyConfig)) {
-        if (proxyConfig.lpszProxy) {
-            wxString proxy(proxyConfig.lpszProxy);
-            GlobalFree(proxyConfig.lpszProxy);
-            if (proxyConfig.lpszProxyBypass) GlobalFree(proxyConfig.lpszProxyBypass);
-            if (proxyConfig.lpszAutoConfigUrl) GlobalFree(proxyConfig.lpszAutoConfigUrl);
-            return proxy;
+    WINHTTP_CURRENT_USER_IE_PROXY_CONFIG ieConfig = {};
+    if (WinHttpGetIEProxyConfigForCurrentUser(&ieConfig)) {
+        WINHTTP_AUTOPROXY_OPTIONS autoProxyOptions = {};
+        autoProxyOptions.fAutoLogonIfChallenged = TRUE;
+
+        if (ieConfig.lpszAutoConfigUrl) {
+            autoProxyOptions.dwFlags = WINHTTP_AUTOPROXY_CONFIG_URL;
+            autoProxyOptions.lpszAutoConfigUrl = ieConfig.lpszAutoConfigUrl;
+        } else if (ieConfig.fAutoDetect) {
+            autoProxyOptions.dwFlags = WINHTTP_AUTOPROXY_AUTO_DETECT;
+            autoProxyOptions.dwAutoDetectFlags = WINHTTP_AUTO_DETECT_TYPE_DHCP | WINHTTP_AUTO_DETECT_TYPE_DNS_A;
         }
-        if (proxyConfig.lpszProxyBypass) GlobalFree(proxyConfig.lpszProxyBypass);
-        if (proxyConfig.lpszAutoConfigUrl) GlobalFree(proxyConfig.lpszAutoConfigUrl);
+
+        WINHTTP_PROXY_INFO winProxyInfo = {};
+        HINTERNET hSession = WinHttpOpen(L"ProxyResolver", WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,
+                                         WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0);
+
+        if (hSession &&
+            WinHttpGetProxyForUrl(hSession, L"https://wms.geo.admin.ch", &autoProxyOptions, &winProxyInfo)) {
+            if (winProxyInfo.lpszProxy)
+                proxyInfo = wxString(winProxyInfo.lpszProxy);
+
+            // Clean up WINHTTP_PROXY_INFO
+            if (winProxyInfo.lpszProxy)
+                GlobalFree(winProxyInfo.lpszProxy);
+            if (winProxyInfo.lpszProxyBypass)
+                GlobalFree(winProxyInfo.lpszProxyBypass);
+        } else {
+            wxLogInfo("WinHttpGetProxyForUrl failed. No proxy used or PAC returned DIRECT.");
+        }
+
+        if (hSession)
+            WinHttpCloseHandle(hSession);
+
+        // Clean up WINHTTP_CURRENT_USER_IE_PROXY_CONFIG
+        if (ieConfig.lpszAutoConfigUrl)
+            GlobalFree(ieConfig.lpszAutoConfigUrl);
+        if (ieConfig.lpszProxy)
+            GlobalFree(ieConfig.lpszProxy);
+        if (ieConfig.lpszProxyBypass)
+            GlobalFree(ieConfig.lpszProxyBypass);
     }
-    return wxEmptyString;
 #else
     // For other platforms, we can return an empty string or handle it differently
-    return wxEmptyString;
 #endif
+
+    return proxyInfo;
 }
 
 wxString GetProxy() {
